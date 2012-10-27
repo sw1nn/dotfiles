e32b8fad55063a02fd9a648b2f2cb3d02f1a6f38
Neale Swinnerton <neale@isismanor.com>
2012-10-27 08:47:20 +0100

    Add leiningen config

---
+++ .lein/profiles.clj
@@ -0,0 +1,5 @@
+{:user {:plugins [[lein-noir "1.2.1"]
+                  [lein-ring "0.5.4"]
+                  [lein-ritz "0.5.0"]
+                  [org.clojars.technomancy/lein-search "1.0.0-SNAPSHOT"]
+                  [cljs-template "0.1.0"]] }}
