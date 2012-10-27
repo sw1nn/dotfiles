acd616220376291a434928f7f6f4a65d1fcbed02
Neale Swinnerton <neale@isismanor.com>
2012-10-27 19:54:43 +0100

    de-cruftify customize-able stuff and repeated config.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,14 +1,8 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ; local (non-elpa) stuff's in here
 (add-to-list 'load-path "~/.emacs.d/local" )
-(add-to-list 'load-path "~/dotfiles/submodule/clojure-mode")
 
-;;;;;;;;;;;;;;;;;core;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; pick a emacs-23 version of package if it's not available
-
-(when (not (require 'package nil t))
-   (require 'package "package-23.el")
-   (package-initialize))
+(package-initialize)
 
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.milkbox.net/packages/"))
@@ -17,50 +11,26 @@
 ; look and feel tweaks
 (add-to-list 'default-frame-alist '(height . 60))
 (add-to-list 'default-frame-alist '(width . 232))
-(global-linum-mode 1)
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-; load local (non-elpa) modules
-
-(require 'auto-complete-config)
-(require 'htmlize)
-(require 'yaml-mode)
-(require 'haml-mode)
-(require 'sass-mode)
-(require 'clojure-mode)
-(require 'clojurescript-mode)
-;(require 'clojure-test-mode)
-(require 'octopress)
-(require 'yasnippet)
-(yas/global-mode 1)
-(setq yas/prompt-functions '(yas/ido-prompt)) 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ; file associations
-(setq auto-mode-alist (cons '("\\.md" . markdown-mode) auto-mode-alist))
-(setq auto-mode-alist (cons '("\\.markdown" . markdown-mode) auto-mode-alist))
-(setq auto-mode-alist (cons '("\\.yml" . yaml-mode) auto-mode-alist))
-(setq auto-mode-alist (cons '("\\.scss" . sass-mode) auto-mode-alist))
-(setq auto-mode-alist (cons '("\\.m" . octave-mode) auto-mode-alist))
+(dolist (mode '(("\\.md" . markdown-mode)
+                ("\\.markdown" . markdown-mode)
+                ("\\.yml" . yaml-mode)
+                ("\\.scss" . sass-mode)
+                ("\\.m" . octave-mode)))
+  (add-to-list 'auto-mode-alist mode))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ; Auto complete config
+(require 'auto-complete-config) 
 (ac-config-default)
 (ac-flyspell-workaround)
-(add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
-(setq ac-comphist-file "~/.emacs.d/ac-comphist.dat")
-
-(global-auto-complete-mode t)
-(setq ac-auto-show-menu t)
-(setq ac-dwim t)
-(setq ac-use-menu-map t)
-(setq ac-quick-help-delay 1)
-(setq ac-quick-help-height 60)
 
 (set-default 'ac-sources
              '(ac-source-dictionary
                ac-source-words-in-buffer
-               ac-source-words-in-same-mode-buffers
-               ac-source-words-in-all-buffer))
+               ac-source-words-in-same-mode-buffers))
 
 (dolist (mode '(magit-log-edit-mode log-edit-mode org-mode text-mode haml-mode
                 sass-mode yaml-mode csv-mode espresso-mode haskell-mode
@@ -68,23 +38,14 @@
                 lisp-mode textile-mode markdown-mode tuareg-mode))
   (add-to-list 'ac-modes mode))
 
-(require 'ac-slime)
-(add-hook 'slime-mode-hook 'set-up-slime-ac)
-(add-hook 'slime-repl-mode-hook 'set-up-slime-ac)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; misc settings
-(setq-default ispell-program-name "/usr/bin/aspell")
-(setq-default ispell-list-command "list")
-
 ;; Load authentication info from an external source.  Put sensitive
      ;; passwords and the like in here.
-(load "~/.emacs.d/.erc-auth")
-
-(require 'erc-services)
-
-(setq erc-nickserv-passwords
-      `((freenode     (("sw1nn" . ,freenode-nickone-pass)))))     
+(let ((auth-file "~/.emacs.d/.erc-auth"))
+  (when (file-exists-p auth-file)
+    (load auth-file)
+    (require 'erc-services)
+    (setq erc-nickserv-passwords
+          `((freenode (("sw1nn" . ,freenode-nickone-pass)))))))     
 
 ;;; Finally, connect to the networks.
 (defun irc-maybe ()
@@ -92,41 +53,21 @@
   (when (y-or-n-p "IRC? ")
     (erc-ssl :server "irc.freenode.net" :port 6697 :nick "sw1nn" :full-name "Neale Swinnerton")))
 
-;; note: may have to amend for unicode project names
-(add-to-list 'same-window-regexps "\\*magit: [[:ascii:]]\\*")
-
-(eval-after-load 'magit
-  '(progn
-     (set-face-foreground 'magit-diff-add "green3")
-     (set-face-foreground 'magit-diff-del "red3")
-     (if (fboundp 'magit-completing-read-function) 
-         (setq magit-completing-read-function 'ido-completing-read))))
-
-(defconst fancy-formatting-defs
-  '(("(\\(fn\\)[\[[:space:]]"
-      (0 (progn (compose-region (match-beginning 1)
-                                (match-end 1) "λ")
-                nil)))
-    ("\\(#\\)("
-     (0 (progn (compose-region (match-beginning 1)
-                               (match-end 1) "ƒ")
-               nil)))
-    ("\\(#\\){"
-     (0 (progn (compose-region (match-beginning 1)
-                               (match-end 1) "∈")
-               nil)))))
-
-(eval-after-load 'clojure-mode
-  '(font-lock-add-keywords
-    'clojure-mode fancy-formatting-defs))
-
-(eval-after-load 'clojurescript-mode
-  '(font-lock-add-keywords
-    'clojurescript-mode fancy-formatting-defs))
-
-(eval-after-load 'nrepl-mode
-  '(font-lock-add-keywords
-    'nrepl-mode 'fancy-formatting-defs))
+(dolist (mode '(clojure-mode clojurescript-mode nrepl-mode))
+  (eval-after-load mode 
+    (font-lock-add-keywords
+     mode '(("(\\(fn\\)[\[[:space:]]"
+             (0 (progn (compose-region (match-beginning 1)
+                                       (match-end 1) "λ")
+                       nil)))
+            ("\\(#\\)("
+             (0 (progn (compose-region (match-beginning 1)
+                                       (match-end 1) "ƒ")
+                       nil)))
+            ("\\(#\\){"
+             (0 (progn (compose-region (match-beginning 1)
+                                       (match-end 1) "∈")
+                       nil)))))))
 
 (defun neale-custom-lisp-mode ()
   (setq cursor-type 'bar)
@@ -141,7 +82,9 @@
   (modify-syntax-entry ?^ "'")
   (modify-syntax-entry ?= "'")
   (paredit-mode t)
-  (flyspell-prog-mode))
+  (flyspell-prog-mode)
+  (yas-minor-mode)
+  (linum-mode))
 
 (defun neale-custom-clojure-mode () 
   (neale-custom-lisp-mode)
@@ -149,28 +92,13 @@
   )
 
 (defun neale-custom-nrepl-mode () 
-  (neale-custom-lisp-mode)
-  (define-key nrepl-mode-map
-    (kbd "DEL") 'paredit-backward-delete)
-  (define-key nrepl-mode-map
-    (kbd "{") 'paredit-open-curly)
-  (define-key nrepl-mode-map
-    (kbd "}") 'paredit-close-curly))
+  (neale-custom-lisp-mode))
 
 (defun neale-custom-inferior-lisp-mode () 
-  (neale-custom-lisp-mode)
-  (define-key inferior-lisp-mode-map
-    (kbd "DEL") 'paredit-backward-delete)
-  (define-key inferior-lisp-mode-map
-    (kbd "{") 'paredit-open-curly)
-  (define-key inferior-lisp-mode-map
-    (kbd "}") 'paredit-close-curly))
+  (neale-custom-lisp-mode))
 
 (add-hook 'nrepl-mode-hook 'ac-nrepl-setup)
 (add-hook 'clojure-nrepl-mode-hook 'ac-nrepl-setup)
-(eval-after-load "auto-complete"
-  '(add-to-list 'ac-modes 'nrepl-mode))
-
 (add-hook 'nrepl-mode-hook 'neale-custom-nrepl-mode)
 (add-hook 'lisp-mode-hook 'neale-custom-lisp-mode)
 (add-hook 'emacs-lisp-mode-hook 'neale-custom-lisp-mode)
@@ -182,6 +110,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ac-auto-show-menu 0.0)
+ '(ac-comphist-file "~/.emacs.d/ac-comphist.dat")
+ '(ac-dictionary-directories (quote ("~/.emacs.d/ac-dict" "/Users/neale/.emacs.d/elpa/auto-complete-20121022.2254/dict")))
  '(blink-matching-paren-on-screen t)
  '(dired-use-ls-dired nil)
  '(erc-autojoin-channels-alist (quote (("freenode.net" "#clojure"))))
@@ -196,10 +127,14 @@
  '(erc-track-exclude-types (quote ("NICK" "PART" "QUIT" "333" "353")))
  '(erc-user-full-name "Neale Swinnerton")
  '(frame-background-mode nil)
+ '(global-auto-complete-mode t)
+ '(global-undo-tree-mode t)
  '(ido-enable-flex-matching t)
  '(inferior-lisp-program "lein repl")
+ '(ispell-program-name "/usr/bin/aspell")
  '(linum-format "%03d ")
  '(recenter-positions (quote (0.2 0.4 0.6 0.8 bottom top)))
+ '(same-window-regexps (quote ("\\*magit: [[:ascii:]]\\*")))
  '(visible-bell nil))
 
 (custom-set-faces
@@ -255,5 +190,3 @@
         (assq-delete-all 'my-keys-minor-mode minor-mode-map-alist)
         (add-to-list 'minor-mode-map-alist mykeys))))
 (ad-activate 'load)
-
-
