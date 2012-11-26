e96da58519e4b9bedc3b21d0cda6b75f4aaa8e61
Neale Swinnerton <neale@isismanor.com>
2012-11-26 12:30:41 +0000

    More jvm memory

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -2,4 +2,6 @@
                   [lein-ring "0.5.4"]
                   [lein-ritz "0.5.0"]
                   [org.clojars.technomancy/lein-search "1.0.0-SNAPSHOT"]
-                  [cljs-template "0.1.0"]] }}
+                  [cljs-template "0.1.0"]] 
+        :jvm-opts ["-Xmx 4G"]}
+ }
