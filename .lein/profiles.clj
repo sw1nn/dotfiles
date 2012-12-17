6828711bb2fe81d56f86ffccd5e0a14c5d804c21
Neale Swinnerton <neale@isismanor.com>
2012-12-17 12:03:28 +0000

    remove some plugins

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,7 +1,3 @@
-{:user {:plugins [[lein-noir "1.2.1"]
-                  [lein-ring "0.5.4"]
-                  [lein-ritz "0.5.0"]
-                  [org.clojars.technomancy/lein-search "1.0.0-SNAPSHOT"]
-                  [cljs-template "0.1.0"]] 
+{:user {:plugins [] 
         :jvm-opts ["-Xmx4G"]}
  }
