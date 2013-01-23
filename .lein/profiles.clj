7dda64943e52e88b9f7225b73309c21c1e8d6ebf
Neale Swinnerton <neale@isismanor.com>
2013-01-23 10:28:44 +0000

    tweaks to default lein profile

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,7 +1,6 @@
-{:user {:plugins [[lein-noir "1.2.1"]
-                  [lein-ring "0.5.4"]
-                  [lein-ritz "0.5.0"]
-                  [org.clojars.technomancy/lein-search "1.0.0-SNAPSHOT"]
-                  [cljs-template "0.1.0"]] 
-        :jvm-opts ["-Xmx4G"]}
+{:user {:plugins []
+        :jvm-opts ["-Xmx4G"]
+        :injections [(require 'clojure.repl)
+                     (require 'clojure.pprint)]
+        :warn-on-reflection true}
  }
