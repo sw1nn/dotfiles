1671fc58b284120c50d35c2cf8d2da10d37920e1
Neale Swinnerton <neale@isismanor.com>
2013-03-06 22:58:46 +0000

    Improved clojure regex special character emacs matching regexp

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -157,7 +157,7 @@
              (0 (progn (compose-region (match-beginning 1)
                                        (match-end 1) "∈")
                        nil)))
-            ("\\(#\\)\""                ; regexes
+            ("\\(#\\)\\(?:\\\".*?\\\"\\)"  ; regexes
              (0 (progn (compose-region (match-beginning 1)
                                        (match-end 1) "®")
                        nil)))))))
