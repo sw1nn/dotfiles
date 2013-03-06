76359560c0cb542e3c0f10463f7f3bdaf296bab3
Neale Swinnerton <neale@isismanor.com>
2013-03-06 12:52:38 +0000

    add ritz

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,6 +1,11 @@
-{:user {:plugins []
+{:user {:plugins [[lein-ritz "0.7.0"]]
         :jvm-opts ["-Xmx4G"]
-        :injections [(require 'clojure.repl)
-                     (require 'clojure.pprint)]
-        :warn-on-reflection true}
- }
+        :injections [(require 'clojure.repl)]
+        :warn-on-reflection true
+        :dependencies [[ritz/ritz-nrepl-middleware "0.7.0"]
+                       [ritz/ritz-debugger "0.7.0"]
+                       [ritz/ritz-repl-utils "0.7.0"]
+                       [clojure-complete "0.2.2"]]
+        :repl-options {:nrepl-middleware 
+                       [ritz.nrepl.middleware.javadoc/wrap-javadoc
+                        ritz.nrepl.middleware.simple-complete/wrap-simple-complete]}}}
