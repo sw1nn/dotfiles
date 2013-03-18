839ae95f16e4406e6259a525f30fd7427a52a46b
Neale Swinnerton <neale@isismanor.com>
2013-03-18 19:35:58 +0000

    typo

--- .emacs.d/conf.d/key-bindings.el
+++ .emacs.d/conf.d/key-bindings.el
@@ -55,7 +55,6 @@
 
 (define-minor-mode my-keys-minor-mode
   "A minor mode so that my key settings override annoying major modes."
-  t " my-keys" 'my-keys-minor-mode-map)
   t " ߐ" 'my-keys-minor-mode-map)
 
 (my-keys-minor-mode 1)
