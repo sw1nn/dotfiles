346850335199fc990c62c7c21b25be09f26ed8c9
Neale Swinnerton <neale@isismanor.com>
2013-03-20 18:08:08 +0000

    add w3m

--- .emacs.d/conf.d/w3m.el
+++ .emacs.d/conf.d/w3m.el
@@ -1,6 +1,8 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; w3m tweaks
 
+(require 'w3m)
+
 (defun w3m-browse-url-other-window (url &optional newwin)
   (let ((w3m-pop-up-windows t))
     (if (one-window-p) (split-window-horizontally))
