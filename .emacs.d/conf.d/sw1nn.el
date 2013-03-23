6a2b9be27bd2c3bcc97044992ddf1a2bab5fda23
Neale Swinnerton <neale@isismanor.com>
2013-03-23 11:05:49 +0000

    Add some utility functions

---
+++ .emacs.d/conf.d/sw1nn.el
@@ -0,0 +1,25 @@
+
+
+(defcustom clj-compile-on-save nil "non-nil means clj files should be compiled after save."  )
+
+(defun toggle-clj-compile-on-save ()
+  (interactive)
+  (setq clj-compile-on-save (not clj-compile-on-save)))
+
+(defun add-clj-compile-on-save ()
+  (add-hook 'after-save-hook
+            (lambda ()
+              (if (and clj-compile-on-save
+                       (symbol-value 'nrepl-interaction-mode))
+                  (progn (message "Compiling...")
+                         (nrepl-load-current-buffer))))))
+
+(defun toggle-fullscreen ()
+  "Toggle full screen on X11"
+  (interactive)
+  (when (eq window-system 'x)
+    (set-frame-parameter
+     nil 'fullscreen
+     (when (not (frame-parameter nil 'fullscreen)) 'fullboth))))
+
+(provide 'sw1nn)
