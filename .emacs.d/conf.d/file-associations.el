179af9cfcb4723d219977d8280d25423abfcd8aa
Neale Swinnerton <neale@isismanor.com>
2013-04-06 14:57:01 +0100

    better emacs style regex for file associations

--- .emacs.d/conf.d/file-associations.el
+++ .emacs.d/conf.d/file-associations.el
@@ -1,11 +1,10 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; file associations
 
-(dolist (mode '(("\\.md\\$" . markdown-mode)
-                ("\\.markdown\\$" . markdown-mode)
-                ("\\.yml\\$" . yaml-mode)
-                ("\\.scss\\$" . sass-mode)
-                ("\\.m\\$" . octave-mode)))
+(dolist (mode '(("\\.md\\'" . markdown-mode)
+                ("\\.markdown\\'" . markdown-mode)
+                ("\\.yml\\'" . yaml-mode)
+                ("\\.scss\\'" . sass-mode)
+                ("\\.m\\'" . octave-mode)))
   (add-to-list 'auto-mode-alist mode))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
