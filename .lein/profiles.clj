2d2218a27f51190cc95d65a6b2d7b87f59516f1d
Neale Swinnerton <neale@isismanor.com>
2013-04-08 16:04:01 +0100

    Add midje

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,11 +1,13 @@
-{:user {:plugins [[lein-ritz "0.7.0"]]
+{:user {:plugins [[lein-ritz "0.7.0"]
+                  [lein-midje "3.0.0"]]
         :jvm-opts ["-Xmx4G"]
         :injections [(require 'clojure.repl)]
-        :warn-on-reflection true
+        ;; :warn-on-reflection true
         :dependencies [[ritz/ritz-nrepl-middleware "0.7.0"]
                        [ritz/ritz-debugger "0.7.0"]
                        [ritz/ritz-repl-utils "0.7.0"]
-                       [clojure-complete "0.2.2"]]
-        :repl-options {:nrepl-middleware 
+                       [clojure-complete "0.2.2"]
+                       ]
+        :repl-options {:nrepl-middleware
                        [ritz.nrepl.middleware.javadoc/wrap-javadoc
                         ritz.nrepl.middleware.simple-complete/wrap-simple-complete]}}}
