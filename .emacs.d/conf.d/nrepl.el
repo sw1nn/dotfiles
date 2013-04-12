45b88deb0711d0ec27dc3ebe1ec72a7271e4e625
Neale Swinnerton <neale@isismanor.com>
2013-04-12 15:32:00 +0100

    make nrepl.el default to port 4001

--- .emacs.d/conf.d/nrepl.el
+++ .emacs.d/conf.d/nrepl.el
@@ -1,10 +1,11 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; nRepl tweaks
 (setq nrepl-javadoc-local-paths (list "/usr/local/share/javadoc-w3m/7/docs/api"))
+(setq-default nrepl-port "4001")
 
-(add-to-list 'same-window-buffer-names "*nrepl*") 
+(add-to-list 'same-window-buffer-names "*nrepl*")
 (add-hook 'nrepl-interaction-mode-hook
           (lambda ()
-            (define-key nrepl-interaction-mode-map (kbd "C-c C-d") 'ac-nrepl-popup-doc)            
+            (define-key nrepl-interaction-mode-map (kbd "C-c C-d") 'ac-nrepl-popup-doc)
             (nrepl-turn-on-eldoc-mode)))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
