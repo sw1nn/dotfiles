f81429d2d7b6ee719306b062445297ad757bbb70
Neale Swinnerton <neale@isismanor.com>
2013-04-15 14:13:53 +0100

    Add awesome spyscope

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,13 +1,14 @@
 {:user {:plugins [[lein-ritz "0.7.0"]
                   [lein-midje "3.0.0"]]
         :jvm-opts ["-Xmx4G"]
-        :injections [(require 'clojure.repl)]
+        :injections [(require 'spyscope.core)
+                     (require 'clojure.repl)]
         ;; :warn-on-reflection true
         :dependencies [[ritz/ritz-nrepl-middleware "0.7.0"]
                        [ritz/ritz-debugger "0.7.0"]
                        [ritz/ritz-repl-utils "0.7.0"]
                        [clojure-complete "0.2.2"]
-                       ]
+                       [spyscope "0.1.3"]]
         :repl-options {:nrepl-middleware
                        [ritz.nrepl.middleware.javadoc/wrap-javadoc
                         ritz.nrepl.middleware.simple-complete/wrap-simple-complete]
