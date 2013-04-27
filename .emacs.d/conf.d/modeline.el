050c4c2fcc829d0ec1810e91eaf6a67e6404a78d
Neale Swinnerton <neale@isismanor.com>
2013-04-27 17:57:57 +0100

    dont shorten major mode in modeline

--- .emacs.d/conf.d/modeline.el
+++ .emacs.d/conf.d/modeline.el
@@ -21,9 +21,6 @@
                   (mode-str (cdr cleaner))
                   (old-mode-str (cdr (assq mode minor-mode-alist))))
              (when old-mode-str
-               (setcar old-mode-str mode-str))
-             ;; major mode
-             (when (eq mode major-mode)
-               (setq mode-name mode-str)))))
+               (setcar old-mode-str mode-str)))))
 
 (add-hook 'after-change-major-mode-hook 'clean-mode-line)
