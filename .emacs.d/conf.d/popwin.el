0a0a3c4c4b81a481b6a37eca83255223046fc553
Neale Swinnerton <neale@isismanor.com>
2013-04-30 12:20:40 +0100

    take emacs-live popwin conf

--- .emacs.d/conf.d/popwin.el
+++ .emacs.d/conf.d/popwin.el
@@ -7,25 +7,52 @@
 
 (require 'popwin-browse-kill-ring)
 
-(push "*Kill Ring*" popwin:special-display-config)
+(setq popwin:special-display-config
+      '(("*Help*"  :height 30)
+        ("*Completions*" :noselect t)
+        ("*Messages*" :noselect t :height 30)
+        ("*compilation*" :noselect t)
+        ("*Backtrace*" :height 30)
+        ("*Messages*" :height 30)
+        ("*Occur*" :noselect t)
+        ("*Ido Completions*" :noselect t :height 30)
+        ("*magit-commit*" :noselect t :height 40 :width 80 :stick t)
+        ("*magit-diff*" :noselect t :height 40 :width 80)
+        ("*magit-edit-log*" :noselect t :height 15 :width 80)
+        ("\\*ansi-term\\*.*" :regexp t :height 30)
+        ("*shell*" :height 30)
+        (".*overtone.log" :regexp t :height 30)
+        ("*gists*" :height 30)
+        ("*sldb.*":regexp t :height 30)
+        ("*nrepl-error*" :height 30 :stick t)
+        ("*nrepl-doc*" :height 30 :stick t)
+        ("*nrepl-src*" :height 30 :stick t)
+        ("*nrepl-result*" :height 30 :stick t)
+        ("*nrepl-macroexpansion*" :height 30 :stick t)
+        ("*Kill Ring*" :height 30)
+        ("*Compile-Log*" :height 30 :stick t)
+        ("*git-gutter:diff*" :height 30 :stick t)))
 
-;; undo-tree
-(push '("*undo-tree*" :width 0.3 :position right) popwin:special-display-config)
+;; (push "*Kill Ring*" popwin:special-display-config)
 
-;; nrepl
-(push "*nrepl-apropos*" popwin:special-display-config)
-(push "*nrepl-macroexpansion*" popwin:special-display-config)
-(push "*nrepl-description*" popwin:special-display-config)
-(push "*nrepl*" popwin:special-display-config)
+;; ;; undo-tree
+;; (push '("*undo-tree*" :width 0.3 :position right) popwin:special-display-config)
 
-(push "*inferior-lisp*" popwin:special-display-config)
+;; ;; nrepl
+;; (push "*nrepl-apropos*" popwin:special-display-config)
+;; (push "*nrepl-macroexpansion*" popwin:special-display-config)
+;; (push "*nrepl-description*" popwin:special-display-config)
+;; (push "*nrepl*" popwin:special-display-config)
 
-;; slime
-(push "*slime-apropos*" popwin:special-display-config)
-(push "*slime-macroexpansion*" popwin:special-display-config)
-(push "*slime-description*" popwin:special-display-config)
-(push '("*slime-compilation*" :noselect t) popwin:special-display-config)
-(push "*slime-xref*" popwin:special-display-config)
-(push '(sldb-mode :stick t) popwin:special-display-config)
-(push 'slime-repl-mode popwin:special-display-config)
-(push 'slime-connection-list-mode popwin:special-display-config)
+;; (push "*inferior-lisp*" popwin:special-display-config)
+
+;; ;; slime
+;; (push "*slime-apropos*" popwin:special-display-config)
+;; (push "*slime-macroexpansion*" popwin:special-display-config)
+;; (push "*slime-description*" popwin:special-display-config)
+;; (push '("*slime-compilation*" :noselect t) popwin:special-display-config)
+;; (push "*slime-xref*" popwin:special-display-config)
+
+;; (push '(sldb-mode :stick t) popwin:special-display-config)
+;; (push 'slime-repl-mode popwin:special-display-config)
+;; (push 'slime-connection-list-mode popwin:special-display-config)
