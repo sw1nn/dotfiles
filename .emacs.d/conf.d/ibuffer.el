d2f6165a9048957575388b6bc560036515fc3958
Neale Swinnerton <neale@isismanor.com>
2013-05-02 19:44:04 +0100

    more space for buffer name in ibuffer

--- .emacs.d/conf.d/ibuffer.el
+++ .emacs.d/conf.d/ibuffer.el
@@ -5,14 +5,14 @@
 
 ;;nicely format the ibuffer and include git-status
 (setq ibuffer-formats '((mark modified read-only git-status-mini " "
-			      (name 18 18 :left :elide)
-			      " "
-			      (size 9 -1 :right)
-			      " "
-			      (git-status 8 8 :left :elide)
-			      " "
-			      (mode 16 16 :left :elide)
-			      " " filename-and-process))
+                              (name 40 40 :left :elide)
+                              " "
+                              (size 9 -1 :right)
+                              " "
+                              (git-status 8 8 :left :elide)
+                              " "
+                              (mode 16 16 :left :elide)
+                              " " filename-and-process))
       ibuffer-show-empty-filter-groups nil)
 
 (add-hook 'ibuffer-hook
