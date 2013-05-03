184bd80688b61e1a57241a25e295536039f2441f
Neale Swinnerton <neale@isismanor.com>
2013-05-03 10:50:02 +0100

    update erc config

--- .emacs.d/conf.d/erc.el
+++ .emacs.d/conf.d/erc.el
@@ -7,8 +7,10 @@
     (load auth-file)
     (require 'erc-services)
     (setq erc-email-user-id freenode-email-user-id)
+    (add-to-list 'erc-networks-alist '(nixhelp "z.je"))
     (setq erc-nickserv-passwords
-          `((freenode (("sw1nn" . ,freenode-nickone-pass)))))))     
+          `((freenode (("sw1nn" . ,freenode-nickone-pass)))
+            (nixhelp  (("qass" . ,nixhelp-nickone-pass)))))))
 
 (require 'erc-highlight-nicknames)
 
@@ -17,18 +19,26 @@
       erc-autojoin-channels-alist '(("freenode.net" "#clojure"))
       erc-hide-list '("JOIN" "NICK" "PART" "QUIT")
       erc-track-exclude-types '("NICK" "PART" "QUIT" "333" "353")
-      erc-modules '(autojoin button completion fill irccontrols 
-			     list match menu move-to-prompt netsplit 
-			     networks noncommands readonly ring 
-			     scrolltobottom services stamp track highlight-nicknames)
-      erc-port 6697
+      erc-modules '(autojoin button completion fill irccontrols
+                             list match menu move-to-prompt netsplit
+                             networks noncommands readonly ring
+                             scrolltobottom services stamp track highlight-nicknames)
+      erc-port 6667
       erc-prompt-for-nickserv-password nil
       erc-prompt-for-password nil
-      erc-scrolltobottom-mode t)
+      erc-scrolltobottom-mode t
+      erc-insert-timestamp-function 'erc-insert-timestamp-left
+      erc-timestamp-format "%H:%M "
+      erc-timestamp-only-if-changed-flag nil
+      erc-hide-timestamps nil)
 
 ;;; Finally, connect to the networks.
 (defun irc-maybe ()
   (interactive)
-  (when (y-or-n-p "IRC? ")
-    (erc-ssl :server "irc.freenode.net" :port 6697 :nick "sw1nn" :full-name "Neale Swinnerton")))
+  (select-frame (make-frame '((name . "IRC Frame")
+                              (minibuffer . t))))
+  (when (y-or-n-p "Freenode? ")
+    (erc-ssl :server "irc.freenode.net" :port 6697 :nick "sw1nn" :full-name "Neale Swinnerton"))
+  (when (y-or-n-p "AA ISP? ")
+    (erc :server "irc.z.je" :nick "qass")))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
