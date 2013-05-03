4faafa5b5ffd2559a9a84b71df12d1fc56d3a5d1
Neale Swinnerton <neale@isismanor.com>
2013-05-03 16:43:47 +0100

    make only nrepl javadocs open in w3m

--- .emacs.d/conf.d/w3m.el
+++ .emacs.d/conf.d/w3m.el
@@ -4,10 +4,16 @@
 (require 'w3m)
 
 (defun w3m-browse-url-other-window (url &optional newwin)
-  (let ((w3m-pop-up-windows t))
-    (if (one-window-p) (split-window-horizontally))
-    (other-window 1)
-    (w3m-browse-url url newwin)))
+  "If the url matches an entry in `nrepl-javadoc-local-paths'
+open the url in w3m otherwise call `browse-url-xdg-open'"
+  (if (find-if (lambda (path)
+                 (string-match (concat "file:" path) url))
+               nrepl-javadoc-local-paths)
+      (let ((w3m-pop-up-windows t))
+        (if (one-window-p) (split-window-horizontally))
+        (other-window 1)
+        (w3m-browse-url url newwin))
+    (browse-url-xdg-open url newwin)))
 
 (setq browse-url-browser-function 'w3m-browse-url-other-window)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
