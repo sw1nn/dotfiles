b3fef456b247d852e1248b74b3c896cd4b17c7cd
Neale Swinnerton <neale@isismanor.com>
2013-05-15 11:44:04 +0100

    w3m defers to OS for default browser

--- .emacs.d/conf.d/w3m.el
+++ .emacs.d/conf.d/w3m.el
@@ -13,7 +13,7 @@ open the url in w3m otherwise call `browse-url-xdg-open'"
         (if (one-window-p) (split-window-horizontally))
         (other-window 1)
         (w3m-browse-url url newwin))
-    (browse-url-xdg-open url newwin)))
+    (browse-url-default-browser url newwin)))
 
 (setq browse-url-browser-function 'w3m-browse-url-other-window)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
