b6a9c2ed48af8da1f3e49e7a7bc9cc1b15ec97b8
Neale Swinnerton <neale@isismanor.com>
2013-07-12 14:57:49 +0100

    rejig autocomplete in nrepl (doesn't work for YAS yet)

--- .emacs.d/conf.d/auto-complete.el
+++ .emacs.d/conf.d/auto-complete.el
@@ -4,7 +4,7 @@
 ;; Auto complete config
 
 (require 'auto-complete)
-(require 'auto-complete-config) 
+(require 'auto-complete-config)
 
 (ac-config-default)
 (add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
@@ -24,8 +24,6 @@
 
 ;; ac-nrepl setup
 (require 'ac-nrepl)
-(add-hook 'nrepl-mode-hook 'ac-nrepl-setup)
-(add-hook 'nrepl-interaction-mode-hook 'ac-nrepl-setup)
 
 (eval-after-load "auto-complete"
   '(add-to-list 'ac-modes 'nrepl-mode))
@@ -34,10 +32,4 @@
 (defun set-auto-complete-as-completion-at-point-function ()
   (setq completion-at-point-functions '(auto-complete)))
 (add-hook 'auto-complete-mode-hook 'set-auto-complete-as-completion-at-point-function)
-
-(add-hook 'nrepl-mode-hook 'set-auto-complete-as-completion-at-point-function)
-(add-hook 'nrepl-interaction-mode-hook 'set-auto-complete-as-completion-at-point-function)
-
-(define-key nrepl-interaction-mode-map (kbd "C-c C-d") 'ac-nrepl-popup-doc)
-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
--- .emacs.d/conf.d/nrepl.el
+++ .emacs.d/conf.d/nrepl.el
@@ -4,8 +4,23 @@
 (setq-default nrepl-port "4001")
 
 (add-to-list 'same-window-buffer-names "*nrepl*")
+
+(defun ensure-yasnippet-is-first-ac-source ()
+  (when (memq 'ac-source-yasnippet ac-sources)
+    (setq ac-sources
+          (cons 'ac-source-yasnippet
+                (remove 'ac-source-yasnippet ac-sources)))))
+(add-hook 'nrepl-interaction-mode-hook 'ac-nrepl-setup)
+(add-hook 'nrepl-interaction-mode-hook 'ensure-yasnippet-is-first-ac-source)
+(add-hook 'nrepl-mode-hook 'ac-nrepl-setup)
+(add-hook 'nrepl-mode-hook 'ensure-yasnippet-is-first-ac-source)
+
 (add-hook 'nrepl-interaction-mode-hook
           (lambda ()
             (define-key nrepl-interaction-mode-map (kbd "C-c C-d") 'ac-nrepl-popup-doc)
             (nrepl-turn-on-eldoc-mode)))
+
+(add-hook 'nrepl-mode-hook 'set-auto-complete-as-completion-at-point-function)
+(add-hook 'nrepl-interaction-mode-hook 'set-auto-complete-as-completion-at-point-function)
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
