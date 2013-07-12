1af6fc11da0d06c8f4fabb4ee23e8c8f585bd361
Neale Swinnerton <neale@isismanor.com>
2013-07-12 14:58:49 +0100

    re-commence battle with ritz - javadoc working

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -9,16 +9,17 @@
                      (require 'alembic.still)]
         ;; :warn-on-reflection true
         :dependencies [
-;                       [ritz/ritz-nrepl-middleware "0.7.0"]
+                       [ritz/ritz-nrepl-middleware "0.7.0"]
 ;                       [ritz/ritz-debugger "0.7.0"]
 ;                       [ritz/ritz-repl-utils "0.7.0"]
                        [clojure-complete "0.2.2"]
                        [spyscope "0.1.3"]
                        [alembic "0.1.0"]
                       ]
-;        :repl-options {:nrepl-middleware
-;                       [ritz.nrepl.middleware.javadoc/wrap-javadoc
-;                        ritz.nrepl.middleware.simple-complete/wrap-simple-complete]
+        :repl-options {:nrepl-middleware
+                       [ritz.nrepl.middleware.javadoc/wrap-javadoc
+                        ritz.nrepl.middleware.simple-complete/wrap-simple-complete]
 ;                       :host "0.0.0.0"
-;                       :port 4001}
+;                       :port 4001
+                       }
  }}
