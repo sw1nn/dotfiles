50489ceecb29b8658b9a983226fa0db4494a24bf
Neale Swinnerton <neale@isismanor.com>
2013-07-17 16:12:33 +0100

    electrify return!!

--- .emacs.d/conf.d/lisp-modes.el
+++ .emacs.d/conf.d/lisp-modes.el
@@ -32,6 +32,22 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; lisp mode tweaks
 
+(defvar electrify-return-match
+    "[\]}\)\"]"
+    "If this regexp matches the text after the cursor, do an \"electric\"
+  return.")
+
+(defun electrify-return-if-match (arg)
+  "If the text after the cursor matches `electrify-return-match' then
+  open and indent an empty line between the cursor and the text.  Move the
+  cursor to the new line."
+  (interactive "P")
+  (let ((case-fold-search nil))
+    (if (looking-at electrify-return-match)
+        (save-excursion (newline-and-indent)))
+    (newline arg)
+    (indent-according-to-mode)))
+
 (defun neale-custom-lisp-mode ()
   (rainbow-delimiters-mode t)
   (paredit-mode t)
