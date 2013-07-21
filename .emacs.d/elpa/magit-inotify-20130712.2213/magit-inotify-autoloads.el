9f3238984bba634420a252667503d993d57157da
Neale Swinnerton <neale@isismanor.com>
2013-07-21 08:33:47 +0100

    magit inotify for linux

---
+++ .emacs.d/elpa/magit-inotify-20130712.2213/magit-inotify-autoloads.el
@@ -0,0 +1,29 @@
+;;; magit-inotify-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (magit-inotify-mode) "magit-inotify" "magit-inotify.el"
+;;;;;;  (20968 63785 0 0))
+;;; Generated autoloads from magit-inotify.el
+
+(autoload 'magit-inotify-mode "magit-inotify" "\
+Refresh status buffer if source tree changes.
+
+\(fn &optional ARG)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("magit-inotify-pkg.el") (20968 63785 64134
+;;;;;;  0))
+
+;;;***
+
+(provide 'magit-inotify-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; magit-inotify-autoloads.el ends here
