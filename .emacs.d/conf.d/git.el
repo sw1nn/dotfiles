836814b5dfa9b163e624d420f81cdfe1e4002b7a
Neale Swinnerton <neale@isismanor.com>
2013-08-12 16:15:32 +0100

    auto fill git comments

--- .emacs.d/conf.d/git.el
+++ .emacs.d/conf.d/git.el
@@ -5,3 +5,8 @@
 (global-git-gutter-mode t)
 
 (setq global-undo-tree-mode t)
+
+(add-hook 'magit-log-edit-mode-hook
+          (lambda ()
+             (set-fill-column 72)
+             (auto-fill-mode 1)))
