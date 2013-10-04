16fcdc0b2568b07ed616b453dce44e0bb494e626
Neale Swinnerton <neale@isismanor.com>
2013-10-04 12:52:09 +0100

    colorful stacktraces in nrepl.

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -4,29 +4,25 @@
                   [lein-try "0.2.0"]
                   ]
         :jvm-opts ["-Xmx4G"]
-        :injections [(try (require 'spyscope.core)
-                       (catch RuntimeException e)) ;; try-catch to workaround lein repl outside workspace
+        :injections [
+                     (try (require 'spyscope.core)
+                          (catch RuntimeException e)) ;; try-catch to workaround lein repl outside workspace
+                     (let [orig (ns-resolve (doto 'clojure.stacktrace require)
+                                            'print-cause-trace)
+                           new (ns-resolve (doto 'clj-stacktrace.repl require)
+                                           'pst)]
+                       (alter-var-root orig (constantly @new)))
                      (require 'clojure.repl)
                      (require 'clojure.pprint)
-                     (require 'alembic.still)]
+                     ]
         ;; :warn-on-reflection true
         :dependencies [
-                       [nrepl-inspect "0.3.0"]
-                       [ritz/ritz-nrepl-middleware "0.7.0"]
-;                       [ritz/ritz-debugger "0.7.0"]
-;                       [ritz/ritz-repl-utils "0.7.0"]
+                       [clj-stacktrace "0.2.5"]
                        [clojure-complete "0.2.3"]
                        [spyscope "0.1.3"]
-                       [alembic "0.2.0"]
                        [slamhound "1.3.3"]
                       ]
         :repl-options {:nrepl-middleware
                        [
-                        inspector.middleware/wrap-inspect
-                        ritz.nrepl.middleware.javadoc/wrap-javadoc
-                        ritz.nrepl.middleware.apropos/wrap-apropos
-                        ritz.nrepl.middleware.simple-complete/wrap-simple-complete]
-;                       :host "0.0.0.0"
-;                       :port 4001
-                       }
- }}
+                        ]
+                       }}}
