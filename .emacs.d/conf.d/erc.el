4f9b2f6084882918dc917ee0bd0c904e82531476
Neale Swinnerton <neale@isismanor.com>
2013-10-04 12:49:31 +0100

    move erc-auth file.

--- .emacs.d/conf.d/erc.el
+++ .emacs.d/conf.d/erc.el
@@ -2,7 +2,7 @@
 ;; ERC Config
 
 ;; Load authentication info from an external source.
-(let ((auth-file "~/.emacs.d/.erc-auth"))
+(let ((auth-file "~/.erc-auth"))
   (when (file-exists-p auth-file)
     (load auth-file)
     (require 'erc-services)
