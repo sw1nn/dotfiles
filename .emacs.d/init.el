06bbd4d0bb15750f7ed909c771d548fe15cfe7dc
Neale Swinnerton <neale@isismanor.com>
2013-10-22 11:49:09 +0100

    Add explanatory comment for after-init-hook.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,1 +1,2 @@
+;; need to do config after (package-initialize). See How Packages work in Emacs 24 - http://www.emacswiki.org/emacs/ELPA#toc4
 (add-hook 'after-init-hook '(lambda nil (load-file (concat user-emacs-directory "my-init.el"))))
