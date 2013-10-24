17561f5335e8c667037e2bde97ca261a8d643ed7
Neale Swinnerton <neale@isismanor.com>
2013-10-24 10:01:27 +0100

    Add default flags for magit commit/tag.

--- .emacs.d/conf.d/git.el
+++ .emacs.d/conf.d/git.el
@@ -4,3 +4,10 @@
 (global-git-gutter+-mode t)
 
 (setq magit-process-popup-time 5)
+
+(setq magit-command-defaults '((logging nil ("--graph") nil)
+                               (committing nil ("--gpg-sign") nil)
+                               (tagging nil ("--sign") nil)))
+(magit-key-mode-generate 'logging)
+(magit-key-mode-generate 'committing)
+(magit-key-mode-generate 'tagging)
