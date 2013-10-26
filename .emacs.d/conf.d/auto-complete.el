9620d1ebacc3b7cbfbf5f5c1b238321bd5a0d0e7
Neale Swinnerton <neale@isismanor.com>
2013-10-26 13:45:12 +0100

    Remove reference to slime.

--- .emacs.d/conf.d/auto-complete.el
+++ .emacs.d/conf.d/auto-complete.el
@@ -15,13 +15,6 @@
       ac-use-menu-map t
       ac-quick-help-delay 1)
 
-;; ac-slime
-;; (require 'ac-slime)
-;; (add-hook 'slime-mode-hook 'set-up-slime-ac)
-;; (add-hook 'slime-repl-mode-hook 'set-up-slime-ac)
-;; (eval-after-load "auto-complete"
-;;   '(add-to-list 'ac-modes 'slime-repl-mode))
-
 ;; ac-nrepl setup
 (require 'ac-nrepl)
 
