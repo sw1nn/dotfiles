cd91c97d058d3c0a90552a4a39f28b5ef86f5c87
Neale Swinnerton <neale@isismanor.com>
2013-10-26 07:21:03 +0100

    Fix mysterious missing clojure-mode files.

--- .emacs.d/elpa/clojure-mode-20131019.624/clojure-mode-autoloads.el
+++ .emacs.d/elpa/clojure-mode-20131019.624/clojure-mode-autoloads.el
@@ -3,9 +3,43 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "clojure-mode" "clojure-mode.el" (21097 12149
-;;;;;;  0 0))
+;;;### (autoloads (clojure-mode) "../../../dotfiles/.emacs.d/elpa/clojure-mode-20131019.624/clojure-mode"
+;;;;;;  "clojure-mode.el" "997ddaed3ee6f052ddd98d14008a4e6d")
 ;;; Generated autoloads from clojure-mode.el
+
+(autoload 'clojure-mode "../../../dotfiles/.emacs.d/elpa/clojure-mode-20131019.624/clojure-mode" "\
+Major mode for editing Clojure code - similar to Lisp mode.
+Commands:
+Delete converts tabs to spaces as it moves back.
+Blank lines separate paragraphs.  Semicolons start comments.
+\\{clojure-mode-map}
+Note that `run-lisp' may be used either to start an inferior Lisp job
+or to switch back to an existing one.
+
+Entry to this mode calls the value of `clojure-mode-hook'
+if that value is non-nil.
+
+\(fn)" t nil)
+
+(add-to-list 'auto-mode-alist '("\\.clj\\'" . clojure-mode))
+
+(add-to-list 'auto-mode-alist '("\\.cljs\\'" . clojure-mode))
+
+(add-to-list 'auto-mode-alist '("\\.cljx\\'" . clojure-mode))
+
+(add-to-list 'auto-mode-alist '("\\.dtm\\'" . clojure-mode))
+
+(add-to-list 'auto-mode-alist '("\\.edn\\'" . clojure-mode))
+
+(add-to-list 'interpreter-mode-alist '("jark" . clojure-mode))
+
+(add-to-list 'interpreter-mode-alist '("cake" . clojure-mode))
+
+;;;***
+
+;;;### (autoloads (clojure-mode) "clojure-mode" "../../../../.emacs.d/elpa/clojure-mode-20131019.624/clojure-mode.el"
+;;;;;;  "997ddaed3ee6f052ddd98d14008a4e6d")
+;;; Generated autoloads from ../../../../.emacs.d/elpa/clojure-mode-20131019.624/clojure-mode.el
 
 (autoload 'clojure-mode "clojure-mode" "\
 Major mode for editing Clojure code - similar to Lisp mode.
@@ -34,6 +68,13 @@ if that value is non-nil.
 (add-to-list 'interpreter-mode-alist '("jark" . clojure-mode))
 
 (add-to-list 'interpreter-mode-alist '("cake" . clojure-mode))
+
+;;;***
+
+;;;### (autoloads nil nil ("../../../../.emacs.d/elpa/clojure-mode-20131019.624/clojure-mode-autoloads.el"
+;;;;;;  "../../../../.emacs.d/elpa/clojure-mode-20131019.624/clojure-mode-pkg.el"
+;;;;;;  "../../../../.emacs.d/elpa/clojure-mode-20131019.624/clojure-mode.el"
+;;;;;;  "clojure-mode.el") (21099 24285 892587 717000))
 
 ;;;***
 
