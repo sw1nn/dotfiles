8348653e3f90651bce0f05df53291b4d1bd3186f
Neale Swinnerton <neale@isismanor.com>
2013-10-26 07:21:45 +0100

    remove typo.

--- .emacs.d/conf.d/zz-paisley-tweak.el
+++ .emacs.d/conf.d/zz-paisley-tweak.el
@@ -1,3 +1,3 @@
 (when (string= system-name "paisley.local")
   (set-face-attribute
-          'default nil :background "#002b36" :foreground "#657b83"))x
+          'default nil :background "#002b36" :foreground "#657b83"))
