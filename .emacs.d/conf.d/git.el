e820911a2833742eae8f0602e3484c41b1bcc87d
Neale Swinnerton <neale@isismanor.com>
2013-12-11 22:13:02 +0000

    Turn of global-hl-line-mode for magit status.

--- .emacs.d/conf.d/git.el
+++ .emacs.d/conf.d/git.el
@@ -14,3 +14,9 @@
 
 ;; work better with feature/whatever branch names from hubflow.
 (setq magit-default-tracking-name-function (lambda (_ branch) branch))
+
+;; magit has it's own highlighting which conflicts with global-hl-line-mode.
+(add-hook 'magit-status-mode-hook
+          (lambda nil
+            (make-local-variable 'global-hl-line-mode)
+            (setq global-hl-line-mode nil)))
