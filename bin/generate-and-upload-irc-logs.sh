4ec97a81dbaf990bead28ce15ae32d66b03f0a77
Neale Swinnerton <neale@isismanor.com>
2014-02-07 20:14:40 +0000

    Improvements to handle timestamps and multiple inputs.

--- bin/generate-and-upload-irc-logs.sh
+++ bin/generate-and-upload-irc-logs.sh
@@ -1,23 +1,33 @@
 #!/bin/sh
 
-exec >& ${HOME}/irc-upload.log 
+#exec >& ${HOME}/irc-upload.log 
 
 DIR=$(mktemp -d)
-
 trap "rm -rf ${DIR}" EXIT QUIT TERM
 
-OUT="${DIR}/kixi-"
+DESTDIR=/var/cache/irclog-upload
+
+OUT="${DIR}/kixi"
 
 echo "${OUT}"
 
-
-/home/neale/bin/split-irssi-logs "$OUT" "/home/neale/irclogs/Freenode/#kixi.log"
+for f in /home/neale/irclogs/Freenode/\#kixi*.log 
+do
+    /home/neale/bin/split-irssi-logs "$OUT" "$f"
+done
 
 logs2html "${DIR}"
 
-cat > ${DIR}/robots.txt << EOF
+if [ ! -f ${DESTDIR}/robots.txt ]; then
+    cat > ${DESTDIR}/robots.txt << EOF
 User-agent: *
 Disallow: /
 EOF
 
-aws --profile mastodonc s3 sync ${DIR}/ s3://irc-logs
+fi 
+
+# We generate into a tmp directory and then only copy over stuff that's
+# changed. All this purely to preserve timestamps.
+rsync -cvrl ${DIR}/ ${DESTDIR}
+
+aws --profile mastodonc s3 sync --delete ${DESTDIR}/ s3://irc-logs
