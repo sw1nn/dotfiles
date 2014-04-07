02e2be057e96d511abbcb2ac056912a177ee2c18
Neale Swinnerton <neale@isismanor.com>
2014-04-07 17:58:11 +0100

    disable ac-repl - too many hangs, too little time.

--- .emacs.d/conf.d/auto-complete.el
+++ .emacs.d/conf.d/auto-complete.el
@@ -1,4 +1,5 @@
-(require 'ac-nrepl)
+;; (require 'ac-nrepl)
+
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Auto complete config
@@ -10,19 +11,19 @@
 (add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
 (ac-flyspell-workaround)
 
-(setq ac-auto-show-menu t
-      ac-dwim t
-      ac-use-menu-map t
-      ac-quick-help-delay 1)
+;; (setq ac-auto-show-menu t
+;;       ac-dwim t
+;;       ac-use-menu-map t
+;;       ac-quick-help-delay 1)
 
 ;; ac-nrepl setup
-(require 'ac-nrepl)
+;; (require 'ac-nrepl)
 
-(eval-after-load "auto-complete"
-  '(add-to-list 'ac-modes 'cider-repl-mode))
+;; (eval-after-load "auto-complete"
+;;   '(add-to-list 'ac-modes 'cider-repl-mode))
 
-;; hook AC into completion-at-point
-(defun set-auto-complete-as-completion-at-point-function ()
-  (setq completion-at-point-functions '(auto-complete)))
-(add-hook 'auto-complete-mode-hook 'set-auto-complete-as-completion-at-point-function)
+;; ;; hook AC into completion-at-point
+;; (defun set-auto-complete-as-completion-at-point-function ()
+;;   (setq completion-at-point-functions '(auto-complete)))
+;; (add-hook 'auto-complete-mode-hook 'set-auto-complete-as-completion-at-point-function)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
