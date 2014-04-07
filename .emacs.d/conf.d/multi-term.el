c0ab8c4bc8162b18692ba0799697c90e2fc5d460
Neale Swinnerton <neale@isismanor.com>
2014-04-07 17:55:52 +0100

    Add a helpful comment for future .emacs.d miners.

--- .emacs.d/conf.d/multi-term.el
+++ .emacs.d/conf.d/multi-term.el
@@ -6,6 +6,11 @@
       default-process-coding-system '(utf-8-unix . utf-8-unix)
       system-uses-terminfo nil)
 
+;; (setq system-uses-terminfo nil) often doesn't work. not sure why,
+;; so try a variant of this command at the shell.
+;;
+;; $ tic -o ~/.terminfo /Applications/Emacs.app/Contents/Resources/etc/e/eterm-color.ti
+
 (defun term-send-tab ()
   "Send tab in term mode."
   (interactive)
