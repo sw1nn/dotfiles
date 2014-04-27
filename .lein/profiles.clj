99521ab2cd6e85e73adf48dddb1b11f1c4caf374
Neale Swinnerton <neale@isismanor.com>
2014-04-27 21:28:05 +0100

    stuff for newer version of cider.

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,24 +1,24 @@
-{:user {:plugins [[lein-ritz "0.7.0"]
-                  [lein-midje "3.0.0"]
-                  [lein-marginalia "0.7.1"]
-                  [lein-try "0.2.0"]
-                  [lein-ancient "0.5.4"]
-                  [jonase/eastwood "0.1.0"]
-                  ]
-        :jvm-opts ["-Xmx4G"]
-        :injections [
-                     (try (require 'spyscope.core)
-                          (catch RuntimeException e)) ;; try-catch to workaround lein repl outside workspace
-                     (let [orig (ns-resolve (doto 'clojure.stacktrace require)
-                                            'print-cause-trace)
-                           new (ns-resolve (doto 'clj-stacktrace.repl require)
-                                           'pst)]
-                       (alter-var-root orig (constantly @new)))
-                     (require 'clojure.repl)
-                     (require 'clojure.pprint)
-                     (require 'alembic.still)
-                     (require 'pjstadig.humane-test-output)
-                     ]
+{:user {:plugins      [[lein-ritz "0.7.0"]
+                       [lein-midje "3.0.0"]
+                       [lein-marginalia "0.7.1"]
+                       [lein-try "0.2.0"]
+                       [lein-ancient "0.5.4"]
+                       [jonase/eastwood "0.1.0"]
+                       ]
+        :jvm-opts     ["-Xmx4G"]
+        :injections   [
+                       (try (require 'spyscope.core)
+                            (catch RuntimeException e)) ;; try-catch to workaround lein repl outside workspace
+                       (let [orig (ns-resolve (doto 'clojure.stacktrace require)
+                                              'print-cause-trace)
+                             new (ns-resolve (doto 'clj-stacktrace.repl require)
+                                             'pst)]
+                         (alter-var-root orig (constantly @new)))
+                       (require 'clojure.repl)
+                       (require 'clojure.pprint)
+                       (require 'alembic.still)
+                       (require 'pjstadig.humane-test-output)
+                       ]
         ;; :warn-on-reflection true
         :dependencies [
                        [clj-stacktrace "0.2.5"]
@@ -27,8 +27,12 @@
                        [spyscope "0.1.3"]
                        [slamhound "1.3.3"]
                        [alembic "0.2.0"]
-                      ]
+                       [cider/cider-nrepl "0.6.0"]
+                       ]
+
         :repl-options {:nrepl-middleware
-                       [
-                        ]
-                       }}}
+                       [cider.nrepl.middleware.classpath/wrap-classpath
+                        cider.nrepl.middleware.complete/wrap-complete
+                        cider.nrepl.middleware.info/wrap-info
+                        cider.nrepl.middleware.inspect/wrap-inspect
+                        cider.nrepl.middleware.stacktrace/wrap-stacktrace]}}}
