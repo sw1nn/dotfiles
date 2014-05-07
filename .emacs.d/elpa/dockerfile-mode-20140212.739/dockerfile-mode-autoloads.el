5676fdfba32e92343e4b966e21c766992a667bc8
Neale Swinnerton <neale@isismanor.com>
2014-05-07 16:56:28 +0100

    mega package upgrade.

--- .emacs.d/elpa/dockerfile-mode-20140212.739/dockerfile-mode-autoloads.el
+++
@@ -1,23 +0,0 @@
-;;; dockerfile-mode-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "dockerfile-mode" "dockerfile-mode.el" (21305
-;;;;;;  19311 0 0))
-;;; Generated autoloads from dockerfile-mode.el
-
-(autoload 'dockerfile-mode "dockerfile-mode" "\
-A major mode to edit Dockerfiles.
-\\{dockerfile-mode-map}
-
-\(fn)" t nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; dockerfile-mode-autoloads.el ends here
--- .emacs.d/elpa/org-trello-20140328.938/org-trello-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "org-trello" "20140328.938" "Minor mode to synchronize org-mode buffer and trello board" '((dash "2.5.0") (request "0.2.0") (elnode "0.9.9.7.6") (esxml "0.3.0") (s "1.7.0") (kv "0.0.19")))
