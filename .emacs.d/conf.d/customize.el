7471e9d88c59ef80cffb63f408803785350b4ff2
Neale Swinnerton <neale@isismanor.com>
2014-06-05 17:52:08 +0100

    vanity is a sin...

--- .emacs.d/conf.d/customize.el
+++ .emacs.d/conf.d/customize.el
@@ -3,7 +3,15 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(initial-scratch-message ";;    _____           __
+;;   /  ___|         /  |
+;;   \\ `--.__      __`| | _ __  _ __
+;;    `--. \\ \\ /\\ / / | || '_ \\| '_ \\
+;;   /\\__/ /\\ V  V / _| || | | | | | |
+;;   \\____/  \\_/\\_/  \\___/_| |_|_| |_|
+
+
+"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
