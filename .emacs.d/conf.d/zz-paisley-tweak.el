ec448ef576f566f0476106671f35aa51b86c3fae
Neale Swinnerton <neale@isismanor.com>
2014-06-05 18:46:06 +0100

    change the tweaks for paisley.

--- .emacs.d/conf.d/zz-paisley-tweak.el
+++ .emacs.d/conf.d/zz-paisley-tweak.el
@@ -1,3 +1,1 @@
-(when (string= system-name "paisley")
-  (set-face-attribute
-          'default nil :height 120 :background "#002b36" :foreground "#657b83"))
+(when (string= system-name "paisley"))
