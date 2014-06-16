75b5a38a2e6f17bf0e9e104f8c8e349f78985b66
Neale Swinnerton <neale@isismanor.com>
2014-06-16 16:01:32 +0100

    clear out old crap that causes trouble

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,38 +1,17 @@
-{:user {:plugins      [[lein-ritz "0.7.0"]
-                       [lein-midje "3.1.3"]
-                       [lein-marginalia "0.7.1"]
-                       [lein-try "0.4.1"]
+{:user {:plugins      [[lein-midje "3.1.3"]
                        [lein-ancient "0.5.5"]
                        [jonase/eastwood "0.1.2"]
-                       ]
-        :jvm-opts     ["-Xmx4G"]
-        :injections   [
-                       (try (require 'spyscope.core)
-                            (catch RuntimeException e)) ;; try-catch to workaround lein repl outside workspace
-                       (let [orig (ns-resolve (doto 'clojure.stacktrace require)
-                                              'print-cause-trace)
-                             new (ns-resolve (doto 'clj-stacktrace.repl require)
-                                             'pst)]
-                         (alter-var-root orig (constantly @new)))
-                       (require 'clojure.repl)
-                       (require 'clojure.pprint)
-                       (require 'alembic.still)
-                       (require 'pjstadig.humane-test-output)
-                       ]
+                       [cider/cider-nrepl "0.7.0-SNAPSHOT"]]
+        :jvm-opts     ["-Xmx4G" "-Djava.net.preferIPv4Stack=true"]
+        :injections   [(require 'clojure.pprint)
+                       (require 'pjstadig.humane-test-output)]
         ;; :warn-on-reflection true
-        :dependencies [
-                       [clj-stacktrace "0.2.7"]
-                       [clojure-complete "0.2.3"]
-                       [pjstadig/humane-test-output "0.6.0"]
-                       [spyscope "0.1.4"]
-                       [slamhound "1.5.5"]
-                       [alembic "0.2.1"]
-                       [cider/cider-nrepl "0.7.0-SNAPSHOT"]
-                       ]
+        :dependencies [[pjstadig/humane-test-output "0.6.0"]]
+        }
 
-        :repl-options {:nrepl-middleware
-                       [cider.nrepl.middleware.classpath/wrap-classpath
-                        cider.nrepl.middleware.complete/wrap-complete
-                        cider.nrepl.middleware.info/wrap-info
-                        cider.nrepl.middleware.inspect/wrap-inspect
-                        cider.nrepl.middleware.stacktrace/wrap-stacktrace]}}}
+  :auth {:repository-auth {#"https://clojars.org/repo"
+                            {:username :env/clojars_username
+                             :password :env/clojars_password}}
+                           #"s3p://mc-maven-repo/"
+                            {:username :env/mc_aws_username
+                             :passphrase :env/mc_aws_passphrase}}}
