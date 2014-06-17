e2e251d9fd222dca810b05287e8fce8fac435df9
Neale Swinnerton <neale@isismanor.com>
2014-06-17 10:15:39 +0100

    package cleanup

--- .emacs.d/elpa/company-cider-20140201.1017/company-cider-autoloads.el
+++
@@ -1,30 +0,0 @@
-;;; company-cider-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads (company-cider) "company-cider" "../../../../../.emacs.d/elpa/company-cider-20140201.1017/company-cider.el"
-;;;;;;  "55c713b516c28a406cd610abd30acf8a")
-;;; Generated autoloads from ../../../../../.emacs.d/elpa/company-cider-20140201.1017/company-cider.el
-
-(autoload 'company-cider "company-cider" "\
-
-
-\(fn COMMAND &optional ARG &rest IGNORED)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("../../../../../.emacs.d/elpa/company-cider-20140201.1017/company-cider-pkg.el"
-;;;;;;  "../../../../../.emacs.d/elpa/company-cider-20140201.1017/company-cider.el")
-;;;;;;  (21389 62763 38677 756000))
-
-;;;***
-
-(provide 'company-cider-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; company-cider-autoloads.el ends here
--- .emacs.d/elpa/company-cider-20140201.1017/company-cider-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "company-cider" "20140201.1017" "company-mode completion back-end for Cider" (quote ((company "0.6.13") (cider "0.6.0"))))
