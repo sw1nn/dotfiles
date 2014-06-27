48932d424be95e6d93b35cef28c2fb1d38b361b6
Neale Swinnerton <neale@isismanor.com>
2014-06-27 11:03:36 +0100

    Vagrant in emacs! They said it couldn't be done.

--- .emacs.d/conf.d/key-bindings.el
+++ .emacs.d/conf.d/key-bindings.el
@@ -60,6 +60,9 @@
 (define-key my-keys-minor-mode-map (kbd "C-c f") 'fold-dwim-toggle)
 (define-key my-keys-minor-mode-map (kbd "C-c s") 'sw1nn-ag-search)
 (define-key my-keys-minor-mode-map (kbd "C-c :") 'sw1nn-cider-run-tests)
+(define-key my-keys-minor-mode-map (kbd "C-c C-v u") 'vagrant-up)
+(define-key my-keys-minor-mode-map (kbd "C-c C-v h") 'vagrant-halt)
+(define-key my-keys-minor-mode-map (kbd "C-c C-v s") 'vagrant-status)
 (when (eq system-type 'darwin)
   (load-file (concat user-emacs-directory "conf.d/google-music-key-bodge.el")))
 
