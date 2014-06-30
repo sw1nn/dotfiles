091eef22867ae37fd7fa56fc103062f0fb5a22e1
Neale Swinnerton <neale@isismanor.com>
2014-06-30 10:28:55 +0100

    bumber package upgrade.

---
+++ .emacs.d/elpa/s-20140620.1657/s-autoloads.el
@@ -0,0 +1,18 @@
+;;; s-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("s-pkg.el" "s.el") (21424 9087 268914
+;;;;;;  754000))
+
+;;;***
+
+(provide 's-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; s-autoloads.el ends here
