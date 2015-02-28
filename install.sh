8915af5396f5372b6b65575f6a5cdfcdd9977b9d
Neale Swinnerton <neale@isismanor.com>
2015-02-28 12:45:58 +0000

    Remove legacy bash.

--- install.sh
+++ install.sh
@@ -19,9 +19,6 @@ do
     link_with_backup "${dotfile}" >> ${LOGFILE}
 done
 
-cecho "magenta" "Sourcing os specific stuff, just in case"
-. ~/.bashrc-os-specfic
-
 cecho "yellow" "Installing ELPA packages..."
 (install_elpa) 2>&1 >> "${LOGFILE}"
 
@@ -29,8 +26,4 @@ cecho "cyan" "Installing local bin"
 mkdir -p ~/bin
 ln -sf "${DOTFILES}/vimpager/vimpager" ~/bin/
 
-read -p "Finished, review log file? [nY]" yes
-
-if [ "${yes}" = "Y" -o -z "${yes}" ]; then
-    less "${LOGFILE}"
-fi    
+cecho "Finished, log is at ${LOGFILE} ..."
