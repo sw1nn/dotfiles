1baa2a4404da7ffc82792ddbefe3ed523e170af1
Neale Swinnerton <neale@isismanor.com>
2015-04-03 10:24:47 +0100

    Better size output for aa quota.

--- bin/aa_quota_left.py
+++ bin/aa_quota_left.py
@@ -2,11 +2,12 @@
 
 from json import loads
 from requests import get
-from hurry.filesize import size,si
+from hurry.filesize import size, si
 
 aa_info = loads(get('https://chaos.aa.net.uk/info?JSON').text)
 
 quota_left = int(aa_info['login'][0]['broadband'][0]['quota_left'])
 quota_amount = int(aa_info['login'][0]['broadband'][0]['quota_monthly'])
 
-print("{0} / {1}".format(size(quota_left, system=si),size(quota_amount,system=si)))
+print("{0} / {1}".format(size(quota_left, system=si),
+                         size(quota_amount, system=si)))
