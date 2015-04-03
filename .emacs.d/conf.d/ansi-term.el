6f3a204f2acec878c18ee2dd5bf10b34080b16ff
Neale Swinnerton <neale@isismanor.com>
2015-04-03 11:11:58 +0100

    Formatting of elisp.

--- .emacs.d/conf.d/ansi-term.el
+++ .emacs.d/conf.d/ansi-term.el
@@ -1,5 +1,11 @@
 ;; line highlighting is pain in the repl.
 (add-hook 'term-mode-hook
-          (lambda nil
-            (make-local-variable 'global-hl-line-mode)
-            (setq global-hl-line-mode nil)))
+	  (function
+	   (lambda ()
+	     (setq term-prompt-regexp "^[^#$%>\n]*[#$%>] *")
+	     (make-local-variable 'mouse-yank-at-point)
+	     (make-local-variable 'transient-mark-mode)
+	     (setq mouse-yank-at-point t)
+	     (setq transient-mark-mode nil)
+	     (auto-fill-mode -1)
+	     (setq tab-width 8 ))))
