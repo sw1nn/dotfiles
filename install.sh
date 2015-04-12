11e253cc8dc2e50bc801eb1ea19dcc72725b3814
Neale Swinnerton <neale@isismanor.com>
2015-04-12 10:08:21 +0100

    update install script for zsh setup.

--- install.sh
+++ install.sh
@@ -1,17 +1,25 @@
-#!/usr/bin/env bash
-
-# Mostly from https://github.com/stuartsierra/dotfiles
+#!/usr/bin/env zsh
 
 set -e
 
-cd $(dirname $0)
-export DOTFILES="$(pwd)"
+export DOTFILES="${0:h}"
 export LOGFILE="${DOTFILES}/install-$(date +'%Y%m%d%H%M%S').log"
+
+exec > >(tee ${LOGFILE})
+exec 2>&1
 
 . "${DOTFILES}/install_functions.sh"
 
 cecho "green" "Updating git submodules..."
-update_submodules 2>&1  >> "${LOGFILE}"
+update_submodules
+
+ln -s ${DOTFILES}/submodule/prezto #{HOME}/.zprezto
+
+setopt EXTENDED_GLOB
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+    ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+done
+
 
 cecho "blue" "Linking dotfiles..."
 ls -A | grep -e  "^\." | grep -v "^\.git$" |grep -v ".gitmodules" | while read dotfile
@@ -19,11 +27,12 @@ do
     link_with_backup "${dotfile}" >> ${LOGFILE}
 done
 
+
+
 cecho "yellow" "Installing ELPA packages..."
-(install_elpa) 2>&1 >> "${LOGFILE}"
+(install_elpa)
 
 cecho "cyan" "Installing local bin"
 mkdir -p ~/bin
-ln -sf "${DOTFILES}/vimpager/vimpager" ~/bin/
 
 cecho "Finished, log is at ${LOGFILE} ..."
