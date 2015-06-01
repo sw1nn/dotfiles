ce03210819c6bc55d5c82663b75980772b3bf7c8
Neale Swinnerton <neale@sw1nn.com>
2015-06-01 23:27:57 +0100

    Update ansi-term handling.

--- .emacs.d/conf.d/ansi-term.el
+++ .emacs.d/conf.d/ansi-term.el
@@ -1,6 +1,7 @@
 (add-hook 'term-mode-hook
           (lambda nil
             ;; line highlighting is pain in the repl.
+            (setq term-prompt-regexp "^[^#$%>\n]*[#$%>⪢] *")
             (make-local-variable 'global-hl-line-mode)
             (setq global-hl-line-mode nil)
             (make-local-variable 'mouse-yank-at-point)
@@ -8,4 +9,15 @@
             (setq mouse-yank-at-point t)
             (setq transient-mark-mode nil)
             (auto-fill-mode -1)
-            (setq tab-width 8 )))
+            (setq tab-width 8 )
+            (yas-minor-mode -1)
+            (goto-address-mode)))
+
+(defadvice term-sentinel (around my-advice-term-sentinel (proc msg))
+  (if (memq (process-status proc) '(signal exit))
+      (let ((buffer (process-buffer proc)))
+        ad-do-it
+        (kill-buffer buffer))
+    ad-do-it))
+
+(ad-activate 'term-sentinel)
