5192fdb84faf2509b6eba5192cc8dee963c8e047
Neale Swinnerton <neale@sw1nn.com>
2016-04-18 08:28:10 +0100

    Updated quota script.

--- bin/aa_quota_left.py
+++ bin/aa_quota_left.py
@@ -3,11 +3,21 @@
 from json import loads
 from requests import get
 from hurry.filesize import size, si
+from os.path import expanduser
+import configparser
+home = expanduser('~')
+
+config = configparser.ConfigParser()
+config.read (home + '/.config/aaisp/config')
+aaisp = config['aaisp']
 
 aa_info = loads(get('https://chaos.aa.net.uk/info?JSON').text)
 
-quota_left = int(aa_info['login'][0]['broadband'][0]['quota_left'])
-quota_amount = int(aa_info['login'][0]['broadband'][0]['quota_monthly'])
+login=[x for x in aa_info['login'] if x['ID'] == aaisp['login']][0]
+broadband=[x for x in login['broadband'] if x['circuit'] == aaisp['circuit']][0]
 
-print("{0} / {1}".format(size(quota_left, system=si),
-                         size(quota_amount, system=si)))
+quota_left = int(broadband['quota_left'])
+quota_amount = int(broadband['quota_monthly'])
+
+print("{0} / {1}".format(size(quota_amount, system=si),
+                         size(quota_left, system=si)))
