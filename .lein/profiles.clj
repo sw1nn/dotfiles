d03ba61788e61a1be3a74cb7125df7ab779f3319
Neale Swinnerton <neale@sw1nn.com>
2016-04-18 07:48:23 +0100

    Various Cider related twiddles

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,20 +1,16 @@
-{:user {:plugins      [[lein-ancient "0.6.4"]
-                       [jonase/eastwood "0.1.4"]
-                       [cider/cider-nrepl "0.8.2"]
-                       [com.palletops/uberimage "0.4.1"]]
-        :jvm-opts     ["-Xmx4G" "-Djava.net.preferIPv4Stack=true"]
-        :injections   [(require 'clojure.pprint)
-                       ;; (require 'pjstadig.humane-test-output)
-                       ;; (pjstadig.humane-test-output/activate!)
-                       ]
+{:user {
+        :plugins      [[org.clojure/tools.nrepl "0.2.12"]
+                       [lein-ancient "0.6.8" :exclusions [org.clojure/clojure] ]
+                       [jonase/eastwood "0.2.3" :exclusions [org.clojure/clojure]]
+                       [cider/cider-nrepl "0.11.0"]
+                       [refactor-nrepl "2.2.0"]
+                       ;; [com.palletops/uberimage "0.4.1" :exclusions [org.clojure/clojure]]
+                       [lein-cljfmt "0.3.0" :exclusions [org.clojure/clojure]]]
+        :jvm-opts     ["-Xmx4G"]
+        :injections   []
         ;; :warn-on-reflection true
-        :dependencies [;; [pjstadig/humane-test-output "0.6.0"]
-                       ]
-        }
-
-  :auth {:repository-auth {#"https://clojars.org/repo"
-                            {:username :env/clojars_username
-                             :password :env/clojars_password}}
-                           #"s3p://mc-maven-repo/"
-                            {:username :env/mc_aws_username
-                             :passphrase :env/mc_aws_passphrase}}}
+        :dependencies [[org.clojure/clojure "1.8.0"]
+                       [acyclic/squiggly-clojure "0.1.5"]
+                       ^:replace [org.clojure/tools.nrepl "0.2.12"]
+                       [criterium "0.4.4"]]
+        :signing {:gpg-key "CCEAE48D"}}}
