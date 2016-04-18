c3d2fb94fe9813cde0ab5eb7c419d5589c9c09f7
Neale Swinnerton <neale@sw1nn.com>
2016-04-18 07:48:39 +0100

    Last attempt at better Xmonad floating windows.

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -20,6 +20,7 @@ import XMonad.Util.WorkspaceCompare(getSortByIndex)
 -- Layouts
 import XMonad.Layout.NoBorders (smartBorders, noBorders)
 import XMonad.Layout.ResizableTile (ResizableTall(..))
+import XMonad.Layout.HintedTile
 import XMonad.Layout.Grid (Grid(..))
 import XMonad.Layout.StackTile(StackTile(..))
 import XMonad.Layout.PerWorkspace (onWorkspace)
@@ -41,30 +42,73 @@ import Data.Ratio ((%))
 import qualified XMonad.StackSet as W
 import qualified Data.Map        as M
 
--- import Graphics.X11.ExtraTypes.XF86
+
+myManageHook =
+  scratchpadManageHook (W.RationalRect 0.4 0.5 0.6 0.4) <+>
+  manageDocks <+>
+  (composeAll . concat $
+   [ [role       =? r --> doFloat       | r <- myRoleFloats]
+   , [className  =? c --> doCenterFloat | c <- myCenterFloats]
+   , [title      =? t --> doFloat       | t <- myTFloats]
+   , [resource   =? r --> doFloat       | r <- myRFloats]
+   , [resource   =? i --> doIgnore      | i <- myIgnores]
+   , [(role      =? x <||> className =? x <||> title =? x <||> resource =? x) --> doShift      "1:web"   | x <- my1Shifts]
+   , [(role      =? x <||> className =? x <||> title =? x <||> resource =? x) --> doShift      "2:edit"  | x <- my2Shifts]
+   , [(role      =? x <||> className =? x <||> title =? x <||> resource =? x) --> doShift      "4:virt"  | x <- my4Shifts]
+   , [(role      =? x <||> className =? x <||> title =? x <||> resource =? x) --> doShiftAndGo "5:gimp"  | x <- my5Shifts]
+   , [(role      =? x <||> className =? x <||> title =? x <||> resource =? x) --> doShift      "6:"      | x <- my6Shifts]
+   , [(role      =? x <||> className =? x <||> title =? x <||> resource =? x) --> doShift      "7:"      | x <- my7Shifts]
+   , [(role      =? x <||> className =? x <||> title =? x <||> resource =? x) --> doShift      "8:im"    | x <- my8Shifts]
+   , [(role      =? x <||> className =? x <||> title =? x <||> resource =? x) --> doShift      "9:music" | x <- my9Shifts]
+   , [(role      =? x <||> className =? x <||> title =? x <||> resource =? x) --> doShift      "3:term"  | x <- my3Shifts]
+   , [isDialog        --> doCenterFloat]
+   ])
+  where
+    doShiftAndGo = doF . liftM2 (.) W.greedyView W.shift
+    myRoleFloats = ["pop-up"]
+    myCenterFloats = ["XGalaga", "Galculator", "Pinentry"]
+    myTFloats = ["RescueTime Offline Time",
+                 "Google+ Hangouts is sharing your screen with plus.google.com.",
+                 "appear.in screen sharing is sharing your screen with appear.in.",
+                 "LastPass: Free Password Manager",
+                 "Hangouts - neale@mastodonc.com"]
+    myRFloats = []
+    myIgnores = ["desktop_window", "Xfce4-notifyd"]
+    my1Shifts = ["browser"]
+    my2Shifts = ["Emacs", "REmacs"]
+    my3Shifts = ["Gnome-terminal", "st-256color"]
+    my4Shifts = ["VirtualBox"]
+    my5Shifts = ["Gimp"]
+    my6Shifts = ["Eagle"]
+    my7Shifts = []
+    my8Shifts = ["Pidgin", "Skype", "Irc", "Slack", "Linphone",
+                 "crx_knipolnnllmklapflnccelgolnpehhpl"]
+    my9Shifts = ["play.google.com__music","Spotify","AudibleManager"]
+    role = stringProperty "WM_WINDOW_ROLE"
 
 -- solarized
 soBackground         = "#002b36"
-soForeground         = "#657b83"
+soForeground         = "#839496"
 soBlack              = "#073642"
-soBrightBlack        = "#002b36"
 soRed                = "#dc322f"
-soBrightRed          = "#cb4b16"
 soGreen              = "#859900"
-soBrightGreen        = "#586e75"
 soYellow             = "#b58900"
-soBrightYellow       = "#657b83"
 soBlue               = "#268bd2"
-soBrightBlue         = "#839496"
 soMagenta            = "#d33682"
-soBrightMagenta      = "#6c71c4"
 soCyan               = "#2aa198"
-soBrightCyan         = "#93a1a1"
 soWhite              = "#eee8d5"
+
+soBrightBlack        = "#002b36"
+soBrightRed          = "#cb4b16"
+soBrightGreen        = "#586e75"
+soBrightYellow       = "#657b83"
+soBrightBlue         = "#839496"
+soBrightMagenta      = "#6c71c4"
+soBrightCyan         = "#93a1a1"
 soBrightWhite        = "#fdf6e3"
 
-colorNormalBorder    = soGreen
-colorFocusedBorder   = soBrightGreen
+colorNormalBorder    = soCyan
+colorFocusedBorder   = soBrightCyan
 
 transBackground      = "[75]" ++ soBackground
 barFont = "aldrich-11"
@@ -78,11 +122,9 @@ statusBarCmd = "dzen2" ++
                " -sa c" ++
                " -fn '" ++ barFont ++ "'" ++
                " -w 1920 -x 0 -y 0 -ta l -expand r -e ''" ++
-               " -xs 2"
+               " -xs 1"
 
--- nwsUrgencyHook = withUrgencyHook dzenUrgencyHook {args = ["-xs", "1", "-fg", soBrightBlue]}
-
-myTerminal           = "gnome-terminal"
+myTerminal           = "st"
 myScratchpadTerminal = "scratchpad"
 
 nwsPP h = defaultPP
@@ -91,7 +133,7 @@ nwsPP h = defaultPP
         , ppVisible = dzenColor soBrightBlue soBackground . wrap "" ""
         , ppSep     = dzenColor soWhite soBackground " ^r(1x8) "
         , ppUrgent  = dzenColor soBackground soYellow . wrap "[" "]"
-        , ppTitle   = dzenColor soWhite "" . trim
+        , ppTitle   = dzenColor soWhite "" . shorten 96
         , ppOutput  = hPutStrLn h
         , ppLayout  = dzenColor soWhite "" .
             (\x -> case x of
@@ -101,9 +143,7 @@ nwsPP h = defaultPP
                 "Full"       -> pad "^i(/home/neale/dotfiles/icons/layout-full.xbm)"
                 _            -> pad $ shorten 10 x)}
 
-------------------------------------------------------------------------
--- Key bindings. Add, modify or remove key bindings here.
---
+
 myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     [ ((modm .|. shiftMask,   xK_Return ), spawn $ myTerminal)
     , ((modm,                 xK_p      ), spawn "yeganesh_run")
@@ -147,122 +187,72 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     , ((modm .|. shiftMask .|. controlMask, xK_s ), spawn "sleep 0.2; scrot -e 'mv $f ~/screenshots/'")
     ]
     ++
-    --
-    -- mod-[1..9], Switch to workspace N
-    -- mod-shift-[1..9], Move client to workspace N
-    --
     [((m .|. modm, k), windows $ f i)
         | (i, k) <- zip (XMonad.workspaces conf) [xK_1 .. xK_9]
         , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]]
     ++
 
-    --
-    -- mod-{w,e,r}, Switch to physical/Xinerama screens 1, 2, or 3
-    -- mod-shift-{w,e,r}, Move client to screen 1, 2, or 3
-    --
     [((m .|. modm, key), screenWorkspace sc >>= flip whenJust (windows . f))
-        | (key, sc) <- zip [xK_e, xK_r, xK_w] [0..]
+        | (key, sc) <- zip [xK_w, xK_r, xK_e] [0..]
         , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
 
-eagle = className =? "Eagle" --> do
-  doShift "6:"
-
-myManageHook = scratchpadManageHook (W.RationalRect 0.4 0.5 0.6 0.4) <+>
-               manageDocks <+>
-               eagle       <+>
-               (composeAll . concat $
-    [ [role       =? r --> doFloat       | r <- myRoleFloats]
-    , [className  =? c --> doCenterFloat | c <- myCenterFloats]
-    , [title      =? t --> doFloat       | t <- myTFloats]
-    , [resource   =? r --> doFloat       | r <- myRFloats]
-    , [resource   =? i --> doIgnore      | i <- myIgnores]
-    , [(role      =? x <||> className =? x <||> title =? x <||> resource =? x) --> doShift      "1:web"   | x <- my1Shifts]
-    , [(role      =? x <||> className =? x <||> title =? x <||> resource =? x) --> doShift      "2:edit"  | x <- my2Shifts]
-    , [(role      =? x <||> className =? x <||> title =? x <||> resource =? x) --> doShift      "4:virt"  | x <- my4Shifts]
-    , [(role      =? x <||> className =? x <||> title =? x <||> resource =? x) --> doShiftAndGo "5:gimp"  | x <- my5Shifts]
-    , [(role      =? x <||> className =? x <||> title =? x <||> resource =? x) --> doShift      "6:"      | x <- my6Shifts]
-    , [(role      =? x <||> className =? x <||> title =? x <||> resource =? x) --> doShift      "7:"      | x <- my7Shifts]
-    , [(role      =? x <||> className =? x <||> title =? x <||> resource =? x) --> doShift      "8:im"    | x <- my8Shifts]
-    , [(role      =? x <||> className =? x <||> title =? x <||> resource =? x) --> doShift      "9:music" | x <- my9Shifts]
-    -- put this at the end in case we have terms with 'custom' class
-    -- picked up above (e.g. Irc)
-    , [(role      =? x <||> className =? x <||> title =? x <||> resource =? x) --> doShift      "3:term"  | x <- my3Shifts]
-    , [isDialog        --> doCenterFloat]
-    ])
-    where
-    doShiftAndGo        = doF . liftM2 (.) W.greedyView W.shift
-    myRoleFloats        = ["pop-up"]
-    myCenterFloats      = ["XGalaga", "Galculator", "Pinentry", "Pavucontrol"]
-    myTFloats           = ["RescueTime Offline Time",
-                           "Google+ Hangouts is sharing your screen with plus.google.com.",
-                           "appear.in screen sharing is sharing your screen with appear.in.",
-                           "LastPass: Free Password Manager",
-                           "Hangouts - neale@mastodonc.com"]
-    myRFloats           = []
-    myIgnores           = ["desktop_window", "kdesktop"]
-    my1Shifts           = ["Google-chrome-stable"]
-    my2Shifts           = ["Emacs", "REmacs"]
-    my3Shifts           = ["Gnome-terminal"]
-    my4Shifts           = ["VirtualBox"]
-    my5Shifts           = ["Gimp"]
-    my6Shifts           = ["Eagle"]
-    my7Shifts           = []
-    my8Shifts           = ["Pidgin", "Skype", "Irc", "HipChat"]
-    my9Shifts           = ["play.google.com__music","Spotify","AudibleManager"]
-    role                = stringProperty "WM_WINDOW_ROLE"
-
 nwsLogHook h = do
-           dynamicLogWithPP $ nwsPP h
+  dynamicLogWithPP $ nwsPP h
 
-myTheme = defaultTheme { decoHeight = 16
-                        , activeColor = "#a6c292"
-                        , activeBorderColor = "#a6c292"
-                        , activeTextColor = "#000000"
-                        , inactiveBorderColor = "#000000"
-                        }
+myTheme =
+  defaultTheme { decoHeight = 16
+               , activeColor = "#a6c292"
+               , activeBorderColor = "#a6c292"
+               , activeTextColor = "#000000"
+               , inactiveBorderColor = "#000000"
+               }
 
-myLayoutHook = onWorkspace "1:web" webL $
-               onWorkspace "2:edit" fullL $
-               onWorkspace "5:gimp" gimpL $
-               onWorkspace "8:im" imL $
-               onWorkspace "9:music" fullL $
-               standardLayouts
-   where
-        standardLayouts =   avoidStruts  $ (tiled |||
-                                            Mirror tiled |||
-                                            Grid |||
-                                            full)
-        --Layouts
-        tiled        = smartBorders (ResizableTall 1 (2/100) (1/2) [])
-        tabLayout    = smartBorders (tabbed shrinkText myTheme)
-        full         = noBorders Full
-        chatLayout   = StackTile 1 (3/100) (1/2)
-        --Im Layout
-        imL = named "Messaging" $
-              avoidStruts $
-              smartBorders $
-              withIM ratio pidginRoster $
-              reflectHoriz $
-              withIM ircRatio irc chatLayout
-          where
-            ratio = (1%7)
+myLayoutHook =
+  onWorkspace "1:web" webL $
+  onWorkspace "2:edit" full $
+  onWorkspace "3:term" termL $
+  onWorkspace "5:gimp" gimpL $
+  onWorkspace "6:aterm" termL $
+  onWorkspace "8:im" imL $
+  onWorkspace "9:music" full $
+  standardLayouts
+  where
+    standardLayouts =
+      (tiled |||
+       Mirror tiled |||
+       full)
+
+    full = noBorders Full
+
+    tiled =
+      (HintedTile 1 (1/10) (1/3) TopLeft Tall)
+
+    webL =
+      tabLayout  ||| tiled ||| full
+
+    termL = standardLayouts ||| Grid
+
+    tabLayout =
+      smartBorders (tabbed shrinkText myTheme)
+
+    chatLayout = StackTile 1 (3/100) (1/2)
+
+    imL =
+      named "Messaging" $
+      withIM ratio hangoutsRoster $
+      reflectHoriz $
+      withIM ircRatio irc chatLayout
+      where ratio = (1%7)
             ircRatio = (1%2)
-            pidginRoster    = And (ClassName "Pidgin") (Role "buddy_list")
+            hangoutsRoster  = And (ClassName "crx_knipolnnllmklapflnccelgolnpehhpl") (Role "app")
             irc  = Title "Irc"
 
-        --Gimp Layout
-        gimpL = named "Gimp" $
-                avoidStruts $
-                smartBorders $
-                withIM (0.11) (Role "gimp-toolbox") $
-                reflectHoriz $
-                withIM (0.15) (Role "gimp-dock") Full
+    gimpL =
+      named "Gimp" $
+      withIM (0.11) (Role "gimp-toolbox") $
+      reflectHoriz $
+      withIM (0.15) (Role "gimp-dock") Full
 
-        --Web Layout
-        webL      = avoidStruts $  tabLayout  ||| tiled ||| full
-
-        --VirtualLayout
-        fullL = avoidStruts $ full
 
 nwsConfig = defaultConfig
        { terminal               = myTerminal
@@ -270,7 +260,7 @@ nwsConfig = defaultConfig
        , modMask                = mod3Mask -- command key
        , focusedBorderColor     = soGreen
        , workspaces             = myWorkspaces
-       , layoutHook             = myLayoutHook
+       , layoutHook             = avoidStruts myLayoutHook
        , keys                   = myKeys
        , manageHook             = myManageHook
        -- , mouseBindings          = smeMouseBindings
@@ -280,4 +270,5 @@ nwsConfig = defaultConfig
 -- Main
 main = do
      h <- spawnPipe statusBarCmd
-     xmonad $ ewmh nwsConfig { logHook = nwsLogHook h }
+     xmonad $ withUrgencyHook NoUrgencyHook
+            $ ewmh nwsConfig { logHook = nwsLogHook h }
