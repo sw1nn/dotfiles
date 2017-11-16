83e56fbd651ae7e6dc01650f9dda9f9c1482c432
Neale Swinnerton <neale@sw1nn.com>
2017-11-16 22:14:01 +0000

    Improve terminal setup.

--- .emacs.d/conf.d/ansi-term.el
+++ .emacs.d/conf.d/ansi-term.el
@@ -6,14 +6,26 @@
             (setq global-hl-line-mode nil)
             (make-local-variable 'mouse-yank-at-point)
             (make-local-variable 'transient-mark-mode)
-            (setq mouse-yank-at-point t)
             (setq transient-mark-mode nil)
             (auto-fill-mode -1)
             (setq tab-width 8 )
             (yas-minor-mode -1)
             (goto-address-mode)))
 
+(add-hook 'shell-mode-hook  'with-editor-export-editor)
+(add-hook 'term-exec-hook   'with-editor-export-editor)
+(add-hook 'eshell-mode-hook 'with-editor-export-editor)
+
+(setenv "TERM" "xterm-256color")
+
+
+(defadvice ansi-term (after advise-ansi-term-coding-system activate)
+  "Force `ansi-term` to be utf-8 after it launches."
+  (set-buffer-process-coding-system 'utf-8-unix 'utf-8-unix))
+
+
 (defadvice term-sentinel (around my-advice-term-sentinel (proc msg))
+  "Kill buffer holding `ansi-term` after process quit."
   (if (memq (process-status proc) '(signal exit))
       (let ((buffer (process-buffer proc)))
         ad-do-it
