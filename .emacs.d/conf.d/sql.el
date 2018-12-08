316d55494d4673a3b3b54c7af6dbf1b0f2e73976
Neale Swinnerton <neale@sw1nn.com>
2018-12-08 07:39:42 +0000

    Use sql-upcase-mode in sql.

--- .emacs.d/conf.d/sql.el
+++ .emacs.d/conf.d/sql.el
@@ -1,3 +1,8 @@
-(require 'sql-upcase)
-
-(add-hook 'sql-mode-hook 'sql-upcase-mode)
+(use-package sqlup-mode
+  :pin melpa-stable
+  :ensure t
+  :config
+  (setq sql-upcase-mixed-case t)
+  :init
+  (add-hook 'sql-mode-hook 'sqlup-mode)
+  (add-hook 'sql-interactive-mode-hook 'sqlup-mode))
