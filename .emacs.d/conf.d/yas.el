c34d5468322c07b930bf0ffd07a3b2a59d4816cb
Neale Swinnerton <neale@sw1nn.com>
2019-10-08 15:45:23 +0100

    Update yas + snippets

--- .emacs.d/conf.d/yas.el
+++ .emacs.d/conf.d/yas.el
@@ -1,6 +1,7 @@
 (use-package yasnippet
-             :ensure t
-	     :pin melpa-stable
-	     :init
-	     ;;(yas-global-mode 1)
-         )
+  :ensure t
+  :pin melpa-stable
+  :init
+  (yas-reload-all)
+  (add-hook 'prog-mode-hook #'yas-minor-mode)
+  )
