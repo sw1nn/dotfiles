dfe7918d8e7fa56a3e37801b11f3fe3aa98b623e
Neale Swinnerton <neale@sw1nn.com>
2020-03-06 21:48:58 +0000

    Allow compilation-read-command nil in .dir-locals.el.

--- .emacs.d/conf.d/look-and-feel.el
+++ .emacs.d/conf.d/look-and-feel.el
@@ -230,6 +230,8 @@
 
 ;; this lets us have a .dir-locals.el turning on rainbow-mode for themes.
 (add-to-list 'safe-local-variable-values '(eval rainbow-mode t))
+(add-to-list 'safe-local-variable-values
+             '(compilation-read-command . nil))
 
 (setq hscroll-margin 2
       hscroll-step 1
