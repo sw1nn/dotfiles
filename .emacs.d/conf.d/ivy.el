ec4d081cd0e57e9946baeb55f33abde9b7fab1d7
Neale Swinnerton <neale@sw1nn.com>
2020-03-28 12:54:36 +0000

    more key binding tweaks

--- .emacs.d/conf.d/ivy.el
+++ .emacs.d/conf.d/ivy.el
@@ -1,6 +1,8 @@
 (use-package ivy
   :ensure t
   :init (ivy-mode 1)
+  :bind (([remap isearch-forward] . swiper)
+	 ([remap isearch-backward] . swiper-backward))
   :config
   (setq ivy-use-virtual-buffers t
         ivy-count-format "(%d/%d) ")
--- .emacs.d/conf.d/key-bindings.el
+++ .emacs.d/conf.d/key-bindings.el
@@ -4,29 +4,17 @@
   :config (which-key-mode)
   :diminish which-key-mode)
 
-(global-set-key (kbd "C-r") #'swiper-isearch-backward)
-(global-set-key (kbd "C-s") #'swiper-isearch)
-
-(global-set-key (kbd "M-%") #'query-replace-regexp)
-(global-set-key (kbd "M-0") #'forward-sexp)
-(global-set-key (kbd "M-1") #'delete-other-windows)
-(global-set-key (kbd "M-9") #'backward-sexp)
-(global-set-key (kbd "M-y") #'browse-kill-ring)
-
-;; (global-set-key (kbd "C-c m")   #'sw1nn/mc-map)
 (global-set-key (kbd "C-c t")   #'sw1nn/ansi-term)
 (global-set-key (kbd "C-c v")   #'revert-buffer)
-
-(global-set-key (kbd "C-x C-b") #'ibuffer)
 
 (global-set-key (kbd "C-+") #'text-scale-increase)
 (global-set-key (kbd "C--") #'text-scale-decrease)
 (global-set-key (kbd "C-0") #'text-scale-reset)
 
-;; remove suspend-frame bindings, too annoying.
-(global-set-key (kbd "C-z") nil)
-(global-set-key (kbd "C-x C-z") nil)
+;; remove suspend-frame bindings, I never want to do this.
+(global-set-key [remap suspend-frame] 'ignore)
 
-(unbind-key "C-x C-c")
+(unbind-key "C-x C-c") ;; too easy to fat finger
+
 (unbind-key "<insert>")
 (define-key global-map [(control insert)] 'overwrite-mode)
