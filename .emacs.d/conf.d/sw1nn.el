f1d8aaf9d723c1c876d78626da03b272d239b237
Neale Swinnerton <neale@sw1nn.com>
2020-03-29 13:43:43 +0100

    Remove (redundant maybe?) clojure syntax tweak.

--- .emacs.d/conf.d/sw1nn.el
+++ .emacs.d/conf.d/sw1nn.el
@@ -48,19 +48,6 @@
         (backward-sexp))
     (paredit-forward)))
 
-;;Treat hyphens as a word character when transposing words
-;; based on https://github.com/overtone/emacs-live/blob/a7951de9bad6153537f6ee8af46d18bbc2bf0166/packs/dev/clojure-pack/config/clojure-conf.el#L39
-(defvar sw1nn/clojure-mode-kebab-word-sep-syntax-table
-  (let ((st (make-syntax-table clojure-mode-syntax-table)))
-    (modify-syntax-entry ?- "w" st)
-    st))
-
-(defun sw1nn/transpose-kebab-words (arg)
-  "Treat hyphens as a word character when transposing words"
-  (interactive "*p")
-  (with-syntax-table sw1nn/clojure-mode-kebab-word-sep-syntax-table
-    (transpose-words arg)))
-
 (defun sw1nn/clj-compilable-file-p (filename)
   (and
    sw1nn/clj-compile-on-save
