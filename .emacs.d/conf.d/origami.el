a1f4e97d61e3d513bef741fde56eff4020feb6f4
Neale Swinnerton <neale@sw1nn.com>
2020-06-03 10:31:43 +0100

    Improve origami bindings.

--- .emacs.d/conf.d/origami.el
+++ .emacs.d/conf.d/origami.el
@@ -3,4 +3,7 @@
 		   :repo "gregsexton/origami.el")
   :bind (:map origami-mode-map
 	      ("C-x f f" . origami-recursively-toggle-node)
-	      ("C-x f u" . origami-undo)))
+	      ("C-x f C-f" . origami-show-only-node)
+	      ("C-x f C-f" . origami-open-all-nodes)
+	      ("C-x f u" . origami-undo)
+	      ("C-x f r" . origami-redo)))
