a4b9d2154ee29745425f73bd6a2b1d68b96d3590
Neale Swinnerton <neale@sw1nn.com>
2020-06-03 10:36:00 +0100

    use system-name correctly.

--- .emacs.d/conf.d/zz-paisley-tweak.el
+++ .emacs.d/conf.d/zz-paisley-tweak.el
@@ -1,3 +1,3 @@
-(when (string= system-name "paisley.local")
+(when (string= (system-name) "paisley.local")
   (set-face-attribute
-          'default nil :background "#002b36" :foreground "#657b83"))
+   'default nil :background "#002b36" :foreground "#657b83"))
