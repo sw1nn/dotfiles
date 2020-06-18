140e0de7c95f296423462fedea5fdc9a6204bd16
Neale Swinnerton <neale@sw1nn.com>
2020-06-18 21:50:37 +0100

    Teach ibuffer about ess help modes.

--- .emacs.d/conf.d/ess.el
+++ .emacs.d/conf.d/ess.el
@@ -1,4 +1,6 @@
 (use-package ess
-  :config (add-to-list 'ibuffer-help-buffer-modes 'ess-help-mode)
+  :config
+  (add-to-list 'ibuffer-help-buffer-modes 'ess-help-mode)
+  (add-to-list 'ibuffer-help-buffer-modes 'ess-r-help-mode)
   :mode ("\\.Rmd\\'" . markdown-mode)
   :pin melpa)
