f32600a62af7d344b724c6f12f89ba4b864d4499
Neale Swinnerton <neale@sw1nn.com>
2020-10-04 13:09:43 +0100

    Update ess R args

--- .emacs.d/conf.d/ess.el
+++ .emacs.d/conf.d/ess.el
@@ -2,5 +2,6 @@
   :config
   (add-to-list 'ibuffer-help-buffer-modes 'ess-help-mode)
   (add-to-list 'ibuffer-help-buffer-modes 'ess-r-help-mode)
+  (setq inferior-R-args "--no-save --no-restore-data")
   :mode ("\\.Rmd\\'" . markdown-mode)
   :pin melpa)
