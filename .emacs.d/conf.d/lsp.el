73410d9501b0076f48ace6799f60a2c4309cd5bf
Neale Swinnerton <neale@sw1nn.com>
2020-10-04 13:10:32 +0100

    lsp simplification

--- .emacs.d/conf.d/lsp.el
+++ .emacs.d/conf.d/lsp.el
@@ -16,34 +16,13 @@
 	lsp-enable-indentation nil
 	lsp-enable-on-type-formatting nil
 	;; lsp-enable-symbol-highlighting nil
+	gc-cons-threshold 100000000
+	read-process-output-max (* 1024 1024)
+	lsp-completion-provider :capf
 	)
-  :quelpa (lsp-mode :fetcher github
-		    :repo "emacs-lsp/lsp-mode"))
-
-(use-package company-lsp
-  :hook (prog-mode . yas-minor-mode)
-  :config
-  (defun company-lsp--rust-completion-snippet (item)
-    "Function providing snippet with the rust language.
-It parses the function's signature in ITEM (a CompletionItem)
-to expand its arguments."
-    (-when-let* ((kind (gethash "kind" item))
-		 (is-function (= kind 3)))
-      (let* ((detail (gethash "detail" item))
-	     (snippet (when (and detail (s-matches? "^\\(pub \\)?\\(unsafe \\)?fn " detail))
-			(-some--> (substring detail (1+ (s-index-of "(" detail)) (s-index-of ")" detail))
-			  (replace-regexp-in-string "^[^,]*self\\(, \\)?" "" it)
-			  (s-split ", " it)
-			  (mapconcat (lambda (x) (format "${%s}" x)) it ", ")))))
-	(concat "(" (or snippet "$1") ")$0"))))
-  :init
-  (setq company-lsp-enable-snippet t)
-  (push 'company-lsp company-backends))
-
+)
 (use-package lsp-ui
   :commands lsp-ui-mode
-  :quelpa (lsp-ui :fetcher github
-		  :repo "emacs-lsp/lsp-ui-mode")
   :bind (:map lsp-mode-map (([remap xref-find-definitions] . lsp-ui-peek-find-definitions)
 			    ([remap xref-find-references] . lsp-ui-peek-find-references)))
   :init (setq lsp-ui-doc-position 'top))
