8240504efeefd24638558bddf4f57300f0189a7d
Neale Swinnerton <neale@sw1nn.com>
2020-11-18 09:22:45 +0000

    Add lsp to more clojure* modes.

--- .emacs.d/conf.d/clojure.el
+++ .emacs.d/conf.d/clojure.el
@@ -1,8 +1,12 @@
 (use-package clojure-mode
   :hook ((clojure-mode . rainbow-delimiters-mode)
 	 (clojure-mode . origami-mode)
-	 (clojure-mode . lsp))
-  :bind (:map clojure-mode-map  (("RET" . electrify-return-if-match))))
+	 (clojure-mode . lsp)
+	 (clojurec-mode . lsp)
+	 (clojurescript-mode . lsp))
+  :bind (:map clojure-mode-map  (("RET" . electrify-return-if-match)))
+  :config
+  (setq  lsp-enable-indentation nil))
 
 (use-package clj-refactor
   :diminish clj-refactor-mode)
