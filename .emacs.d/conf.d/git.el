9e8392084893729e0f506577eea43ed0413769df
Neale Swinnerton <neale@sw1nn.com>
2020-11-18 09:23:40 +0000

    Fix ordering between ibuffer/magit marking fn.

--- .emacs.d/conf.d/git.el
+++ .emacs.d/conf.d/git.el
@@ -9,7 +9,7 @@
 
 (use-package magit
   :bind (("C-c g g" . magit-status)
-	 :map ibuffer-mode-map ("* g" . sw1nn/ibuffer-mark-magit-buffers))
+	 ("C-c g f" . magit-file-popup))
   :hook (((magit-post-stage magit-post-unstage) . git-gutter:update-all-windows)
 	 (after-save . magit-after-save-refresh-status))
   :config
@@ -20,14 +20,7 @@
 	magit-process-popup-time -1
 	magit-push-arguments '("--set-upstream")
 	magit-repo-dirs '("~/workspace")
-	magit-revert-buffers 'silent)
-  (defun sw1nn/ibuffer-mark-magit-buffers ()
-    "Mark buffers whose major mode is magit related."
-    (interactive)
-    (ibuffer-mark-on-buffer
-     #'(lambda (buf)
-	 (with-current-buffer buf
-	   (derived-mode-p 'magit-mode 'magit-popup-mode))))))
+	magit-revert-buffers 'silent))
 
 (setenv "INSIDE_EMACS" (format "%s,comint" emacs-version))
 (pinentry-start)
--- .emacs.d/conf.d/ibuffer.el
+++ .emacs.d/conf.d/ibuffer.el
@@ -3,6 +3,7 @@
 	 (ibuffer . ibuffer-vc-set-filter-groups-by-vc-root)
 	 (ibuffer . ibuffer-do-sort-by-alphabetic)
 	 (ibuffer . all-the-icons-ibuffer-mode))
+  :bind (:map ibuffer-mode-map ("* g" . sw1nn/ibuffer-mark-magit-buffers))
   :config
   (setq ibuffer-formats '((mark modified read-only vc-status-mini " "
 				(icon 2 2 :left :elide)
@@ -15,6 +16,12 @@
                                 (vc-status 16 16 :left :elide)
                                 " "
                                 filename-and-process))
-        ibuffer-show-empty-filter-groups nil))
+        ibuffer-show-empty-filter-groups nil)
+  (defun sw1nn/ibuffer-mark-magit-buffers ()
+    (interactive)
+    (ibuffer-mark-on-buffer
+     #'(lambda (buf)
+	 (with-current-buffer buf
+	   (derived-mode-p 'magit-mode 'magit-popup-mode))))))
 
 (use-package all-the-icons-ibuffer)
