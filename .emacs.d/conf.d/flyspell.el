36a1fbef8257df2f8d6b54a0aa9f87e98a681200
Neale Swinnerton <neale@sw1nn.com>
2020-11-18 09:23:08 +0000

    Make flyspell search for the ispell program.

--- .emacs.d/conf.d/flyspell.el
+++ .emacs.d/conf.d/flyspell.el
@@ -2,5 +2,6 @@
   :hook ((text-mode . flyspell-mode)
   	 (prog-mode . flyspell-prog-mode))
   :config
-  (setq ispell-program-name (executable-find "aspell"))
+  (setq ispell-program-name (or (executable-find "hunspell")
+				(executable-find "aspell")))
   :diminish flyspell-mode)
